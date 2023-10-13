67c9eaad2da8545324c32e079196ff5ca44309a2
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-13 11:32:31 +0200

    Add files via upload

---
+++ tests/utils/seg_to_loop/test_file.py
@@ -0,0 +1,14 @@
+"""
+Test file methods
+"""
+
+from examples.utils.seg_to_loop.ex_file import example_file
+
+
+# class TestFile(unittest.TestCase):
+
+
+def test_file():
+    """ Test example_file """
+
+    example_file()
