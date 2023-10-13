a5b2b5411d192e8f8c1e3cb7a669071e7e81e414
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-13 16:55:55 +0200

    Update __init__.py

--- PySSPFM/__init__.py
+++ PySSPFM/__init__.py
@@ -3,4 +3,6 @@ import settings
 
 ROOT = pathlib.Path(__file__).parent.parent
 EXAMPLES = ROOT / 'examples'
-DATA = EXAMPLES / 'data'
+EXAMPLE_ROOT_PATH_IN = EXAMPLES / 'datas' / 'PySSPFM_example_in'
+EXAMPLE_ROOT_PATH_OUT = EXAMPLES / 'datas' / 'PySSPFM_example_out'
+DEFAULT_DATA_PATH_OUT = EXAMPLES / 'datas' / 'PySSPFM_data_out'
