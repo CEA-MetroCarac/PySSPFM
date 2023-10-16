cba9980dafd9c78de4c38165182fb062d198b9fa
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-16 10:26:45 +0200

    Update __init__.py

--- PySSPFM/__init__.py
+++ PySSPFM/__init__.py
@@ -1,10 +1,3 @@
 import pathlib
-import settings
-
-ROOT = pathlib.Path(__file__).parent.parent
-EXAMPLES = ROOT / 'examples'
-EXAMPLE_ROOT_PATH_IN = EXAMPLES / 'datas' / 'PySSPFM_example_in'
-EXAMPLE_ROOT_PATH_OUT = EXAMPLES / 'datas' / 'PySSPFM_example_out'
-DEFAULT_DATA_PATH_OUT = ROOT / 'PySSPFM_data_out'
-DEFAULT_LOGO_PATH = ROOT / 'PySSPFM' / 'logo_icon' / 'logoPySSPFM.png'
-DEFAULT_ICON_PATH = ROOT / 'PySSPFM' / 'logo_icon' / 'iconPySSPFM.png'
+from settings import \
+(ROOT, EXAMPLES, EXAMPLE_ROOT_PATH_IN, EXAMPLE_ROOT_PATH_OUT, DEFAULT_DATA_PATH_OUT, DEFAULT_LOGO_PATH, DEFAULT_ICON_PATH)
