ba5014edf0d600eebd92d92643fb3c1fe2d12b53
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-16 14:21:52 +0200

    Update hysteresis_clustering.py

--- PySSPFM/gui/hysteresis_clustering.py
+++ PySSPFM/gui/hysteresis_clustering.py
@@ -12,7 +12,7 @@ from datetime import datetime
 
 from PySSPFM.toolbox.hysteresis_clustering import \
     main_hysteresis_clustering as main_script
-from PySSPFM.user_interface.utils import \
+from PySSPFM.gui.utils import \
     (add_separator_grid, grid_item, show_tooltip, extract_var,
      init_secondary_wdw, wdw_main_title)
 from PySSPFM.utils.path_for_runable import save_path_management, save_user_pars
