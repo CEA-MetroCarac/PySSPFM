9ae4f030917230be09a2682f72019527bca73545
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-16 10:45:46 +0200

    Update default_settings.py

--- examples/default_settings.py
+++ examples/default_settings.py
@@ -1,6 +1,17 @@
 """
 All default constants for PySSPFM examples and tests
+Path and option to save datas generated from tests and examples
+(SAVE_TEST_EXAMPLE) can be adjusted
+All other settings must not be modified
 """
+
+import pathlib
+
+EXAMPLES = pathlib.Path(__file__)
+EXAMPLE_ROOT_PATH_IN = EXAMPLES / 'datas' / 'PySSPFM_example_in'
+EXAMPLE_ROOT_PATH_OUT = EXAMPLES / 'datas' / 'PySSPFM_example_out'
+SAVE_TEST_EXAMPLE = True
+
 KEY_MEASUREMENT_EXTRACTION = {
     'spm': {
         'classic': {'time': 'times',
