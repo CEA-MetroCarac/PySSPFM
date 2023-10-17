897092a81baf4040988694f6aea3d2f6e5be408a
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-17 17:12:43 +0200

    Update test_hysteresis_clustering.py

--- tests/toolbox/test_hysteresis_clustering.py
+++ tests/toolbox/test_hysteresis_clustering.py
@@ -90,7 +90,7 @@ def test_hysteresis_clustering():
     assert cluster_info["on"][1][1] == approx(0.00499325920622482)
 
     assert cluster_info["coupled"][0][1] == approx(0.0018836636880818609)
-    assert cluster_info["coupled"][1][1] == approx(0.0018232107281122326)
+    assert cluster_info["coupled"][1][1] == approx(0.0018836636880818609)
     assert cluster_info["coupled"][2][1] == approx(0.002469161284200277)
     assert cluster_info["coupled"][3][1] == approx(0.005585684068363985)
 
