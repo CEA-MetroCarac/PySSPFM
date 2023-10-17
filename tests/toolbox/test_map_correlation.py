87c86370877f8c211a2630833995ff81f073907f
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-17 10:45:14 +0200

    Update test_map_correlation.py

--- tests/toolbox/test_map_correlation.py
+++ tests/toolbox/test_map_correlation.py
@@ -19,6 +19,6 @@ def test_map_correlation():
     # print(np.nansum(coef_arr['off']))
     # print(np.nansum(coef_arr['off on']))
 
-    assert np.nansum(coef_arr['on']) == approx(157.33132034451688)
+    assert np.nansum(coef_arr['on']) == approx(157.2555736840546)
     assert np.nansum(coef_arr['off']) == approx(92.95253375859342)
     assert np.nansum(coef_arr['off on']) == approx(2.8008741872277128)
