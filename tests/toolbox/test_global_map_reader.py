cc33610c5103e048de366dd6d4d3129f837039c2
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-17 10:44:16 +0200

    Update test_global_map_reader.py

--- tests/toolbox/test_global_map_reader.py
+++ tests/toolbox/test_global_map_reader.py
@@ -28,5 +28,5 @@ def test_global_map_reader():
     assert list(mask['coupled']) == coupled_mask_target
 
     assert np.nansum(coef_arr['off']) == approx(92.8133823858753)
-    assert np.nansum(coef_arr['on']) == approx(157.33132034451688)
+    assert np.nansum(coef_arr['on']) == approx(157.2555736840546)
     assert np.nansum(coef_arr['off on']) == approx(2.8008741872277128)
