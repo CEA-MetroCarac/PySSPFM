1a7f4c9ab0ce94c93a7736af0ce2bbc3c9f82fcb
hugoval <76450221+yudgugger@users.noreply.github.com>
2023-10-17 11:28:58 +0200

    Update test_hysteresis_clustering.py

--- tests/toolbox/test_hysteresis_clustering.py
+++ tests/toolbox/test_hysteresis_clustering.py
@@ -63,7 +63,7 @@ def test_hysteresis_clustering():
     assert np.sum(avg_hysteresis["coupled"][1]) == approx(
         0.022249788685527537)
     assert np.sum(avg_hysteresis["coupled"][2]) == approx(
-        -0.0027888739086245396)
+        -0.0027894943553524987)
     assert np.sum(avg_hysteresis["coupled"][3]) == approx(0.01173711573269598)
 
     assert cluster_info["off"][0][0] == approx(0.0)
