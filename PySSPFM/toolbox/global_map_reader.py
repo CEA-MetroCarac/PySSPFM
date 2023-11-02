a53d5070330b2e4bdc38777f5a33673ccdc1ebc5
yudgugger <yudgugger@gmail.com>
2023-11-02 14:00:41 +0100

    rename datas --> data rename loop --> nanoloop rename measurements --> properties adapt many function names change management path for saving file and data in examples checking of global dosctring and comment

--- PySSPFM/toolbox/global_map_reader.py
+++ PySSPFM/toolbox/global_map_reader.py
@@ -1,7 +1,7 @@
 """
 --> Executable Script
-This module is used to generate sspfm maps by extracting data from ferro
-measurements in text files.
+This module is used to generate sspfm maps by extracting data from sample
+properties in text files.
 """
 
 import os
@@ -10,17 +10,17 @@ from datetime import datetime
 
 from PySSPFM.utils.core.figure import print_plots
 from PySSPFM.utils.map.main import main_mapping
-from PySSPFM.utils.nanoloop_to_hyst.file import extract_measures
-from PySSPFM.toolbox.map_correlation import cross_corr_sspfm
+from PySSPFM.utils.nanoloop_to_hyst.file import extract_properties
+from PySSPFM.toolbox.map_correlation import correlation_analysis_all_maps
 from PySSPFM.utils.path_for_runable import save_path_management, save_user_pars
 
 
 def main_global_map_reader(
         user_pars, verbose=False, show_plots=False, save_plots=False,
-        dir_path_in=None, dir_path_out=None, all_meas=None, dim_pix=None,
+        dir_path_in=None, dir_path_out=None, all_prop=None, dim_pix=None,
         dim_mic=None, index_lim=None):
     """
-    Generate all sspfm maps from extraction of ferro measurements in txt files
+    Generate all sspfm maps from extraction of sample properties in txt files
 
     Parameters
     ----------
@@ -33,11 +33,11 @@ def main_global_map_reader(
     save_plots: bool, optional
         Activation key for fig save
     dir_path_in: str, optional
-        Path of ferro measurements txt directory (in)
+        Path of properties txt directory (in)
     dir_path_out: str, optional
         Path of saving directory for sspfm maps (out)
-    all_meas: dict, optional
-        Dict of measurements of sspfm maps
+    all_prop: dict, optional
+        Dict of properties of sspfm maps
     dim_pix: dict('x': ,'y':) of int, optional
         Dict of map dimension for 'x' and 'y' axis (in pixel)
     dim_mic: dict('x': ,'y':) of float, optional
@@ -52,12 +52,12 @@ def main_global_map_reader(
     coef_corr_arr: dict
         All cross correlation coefficient arrays
     """
-    assert dir_path_in is not None or all_meas is not None
+    assert dir_path_in is not None or all_prop is not None
 
     # Directory management
     if save_plots and dir_path_out is None and dir_path_in is not None:
         root, _ = os.path.split(dir_path_in)
-        dir_path_out = os.path.join(root, 'results', 'figs_step_2')
+        dir_path_out = os.path.join(root, 'maps')
         if not os.path.isdir(dir_path_out):
             os.makedirs(dir_path_out)
 
@@ -65,43 +65,43 @@ def main_global_map_reader(
                    'func': user_pars['interp func']}
     lab_tab = [['on', 'off', 'coupled'], ['y', 'w', 'r'],
                ['On Field', 'Off Field', 'Coupled']]
-    measures, mask = {}, {}
+    properties, mask = {}, {}
 
-    # Extract all measurements
+    # Extract all properties
     if dir_path_in is not None:
-        all_meas, dim_pix, dim_mic = extract_measures(dir_path_in)
+        all_prop, dim_pix, dim_mic = extract_properties(dir_path_in)
 
     # Cross correlation analysis between maps
-    coef_corr_arr, figs = cross_corr_sspfm(all_meas)
+    coef_corr_arr, figs = correlation_analysis_all_maps(all_prop)
     print_plots(figs, show_plots=show_plots, save_plots=save_plots,
                 dirname=dir_path_out)
 
-    for mode in all_meas.keys():
+    for mode in all_prop.keys():
         # Ref determination
         if user_pars['man mask'][mode] is None:
-            ref = all_meas[mode][user_pars['ref'][mode]['meas']]
+            ref = all_prop[mode][user_pars['ref'][mode]['prop']]
         else:
             user_pars['ref'][mode], ref = None, None
 
         indx = lab_tab[0].index(mode)
         dict_map = {'label': lab_tab[2][indx], 'col': lab_tab[1][indx]}
-        measures[mode] = {}
+        properties[mode] = {}
 
         # Limit of nb of map plotted
         if index_lim is not None:
-            for cont, key in enumerate(all_meas[mode].keys()):
+            for cont, key in enumerate(all_prop[mode].keys()):
                 if cont < index_lim[0]:
                     continue
                 if index_lim[0] <= cont < index_lim[1]:
-                    measures[mode][key] = all_meas[mode][key]
+                    properties[mode][key] = all_prop[mode][key]
                 else:
                     break
         else:
-            measures[mode] = all_meas[mode]
+            properties[mode] = all_prop[mode]
 
         # Generate map
         mask[mode] = main_mapping(
-            measures[mode], dim_pix, dim_mic=dim_mic, dict_interp=dict_interp,
+            properties[mode], dim_pix, dim_mic=dim_mic, dict_interp=dict_interp,
             dict_map=dict_map, ref=ref, dict_ref=user_pars['ref'][mode],
             mask=user_pars['man mask'][mode], verbose=verbose,
             show_plots=show_plots, save_plots=save_plots,
@@ -132,13 +132,13 @@ def parameters():
         It should be a dictionary with mode keys and lists of indices as values.
         - If the list of pixels is empty ( [] ), all files are selected.
         - If the list of pixels is None, the criterion of selection is made
-        with the reference measurement.
+        with the reference property.
         - If the list of pixels is [a, b, c ...], files of index a, b, c [...]
         are not selected.
     - ref: dict [for each mode] of dict
         --> construct a mask with a criterion selection on ref values
         (valid if man_mask is None)
-        - meas: str --> reference measurement chosen
+        - prop: str --> reference property chosen
         - min val: float --> minimum value of ref required (if None no minimum
         value criterion) (valid if interactive is False)
         - max val: float --> maximum value of ref required (if None no maximum
@@ -148,9 +148,9 @@ def parameters():
         selection
 
     - dir_path_in: str
-        Ferroelectric measurements files directory (default: txt_ferro_meas)
-        This parameter specifies the directory containing the ferroelectric
-        measurements text files generated after the 2nd step of the analysis.
+        Properties files directory (default: properties)
+        This parameter specifies the directory containing the properties text
+        files generated after the 2nd step of the analysis.
     - dir_path_out: str
         Saving directory for analysis results figures
         (optional, default: toolbox directory in the same root)
@@ -170,7 +170,7 @@ def parameters():
         generated during the analysis process.
     """
     dir_path_in = tkf.askdirectory()
-    # dir_path_in = r'...\KNN500n_15h18m02-10-2023_out_dfrt\txt_ferro_meas
+    # dir_path_in = r'...\KNN500n_15h18m02-10-2023_out_dfrt\properties
     dir_path_out = None
     # dir_path_out = r'...\KNN500n_15h18m02-10-2023_out_dfrt\toolbox\
     # global_map_reader_2023-10-02-16h38m
@@ -182,17 +182,17 @@ def parameters():
                  'man mask': {'on': [],
                               'off': [],
                               'coupled': []},
-                 'ref': {'on': {'meas': 'charac tot fit: area',
+                 'ref': {'on': {'prop': 'charac tot fit: area',
                                 'fmt': '.5f',
                                 'min val': None,
                                 'max val': 0.005,
                                 'interactive': False},
-                         'off': {'meas': 'charac tot fit: area',
+                         'off': {'prop': 'charac tot fit: area',
                                  'fmt': '.5f',
                                  'min val': None,
                                  'max val': 0.005,
                                  'interactive': False},
-                         'coupled': {'meas': 'r_2',
+                         'coupled': {'prop': 'r_2',
                                      'fmt': '.5f',
                                      'min val': 0.95,
                                      'max val': None,
