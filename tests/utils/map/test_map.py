a5ad4d8f671abfc2966af9147501f42b7e144378
yudgugger <yudgugger@gmail.com>
2023-11-02 14:24:09 +0100

    rename datas --> data rename loop --> nanoloop rename measurements --> properties adapt many function names change management path for saving file and data in examples checking of global dosctring and comment

--- tests/utils/map/test_map.py
+++ tests/utils/map/test_map.py
@@ -10,9 +10,9 @@ from examples.utils.map.ex_map import example_map
 
 
 def test_map_auto():
-    """ Test example_maps: mask_mode='ref meas: auto' """
+    """ Test example_map: mask_mode='ref prop: auto' """
 
-    mask = example_map(mask_mode='ref meas: auto')
+    mask = example_map(mask_mode='ref prop: auto')
 
     # print(np.sum(mask))
 
@@ -20,7 +20,7 @@ def test_map_auto():
 
 
 def test_map_mask():
-    """ Test example_maps: mask_mode='man mask' """
+    """ Test example_map: mask_mode='man mask' """
 
     mask = example_map(mask_mode='man mask')
 
