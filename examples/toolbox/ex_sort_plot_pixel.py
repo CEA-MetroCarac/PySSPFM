1ffacfda54fdd7cfdcec7e90e5545b2762744ec1
yudgugger <yudgugger@gmail.com>
2023-11-02 13:37:56 +0100

    rename datas --> data rename loop --> nanoloop rename measurements --> properties adapt many function names change management path for saving file and data in examples checking of global dosctring and comment

--- examples/toolbox/ex_sort_plot_pixel.py
+++ examples/toolbox/ex_sort_plot_pixel.py
@@ -1,5 +1,5 @@
 """
-Example of sort_plot methods
+Example of sort_plot_pixel methods
 """
 import os
 
@@ -9,9 +9,9 @@ from PySSPFM.toolbox.sort_plot_pixel import main_sort_plot_pixel
 from PySSPFM import EXAMPLE_ROOT_PATH_IN
 
 
-def example_sort_plot(verbose=False, make_plots=False):
+def example_sort_plot_pixel(verbose=False, make_plots=False):
     """
-    Example of sort_plot functions.
+    Example of sort_plot_pixel functions.
 
     Parameters
     ----------
@@ -27,16 +27,15 @@ def example_sort_plot(verbose=False, make_plots=False):
     """
     dir_path_in = os.path.join(
         EXAMPLE_ROOT_PATH_IN, "KNN500n_2023-10-05-17h23m_out_dfrt")
-    dir_path_in_meas = os.path.join(dir_path_in, "txt_ferro_meas")
-    dir_path_in_loop = os.path.join(dir_path_in, "txt_loops")
-    file_path_in_pars = os.path.join(
-        dir_path_in, "results", "saving_parameters.txt")
+    dir_path_in_props = os.path.join(dir_path_in, "properties")
+    dir_path_in_loop = os.path.join(dir_path_in, "nanoloops")
+    file_path_in_pars = os.path.join(dir_path_in, "parameters.txt")
     user_pars = {'dir path in': dir_path_in,
-                 'dir path in meas': dir_path_in_meas,
+                 'dir path in prop': dir_path_in_props,
                  'dir path in loop': dir_path_in_loop,
                  'file path in pars': file_path_in_pars,
-                 'meas key': {'mode': 'off',
-                              'meas': 'fit pars: ampli_0'},
+                 'prop key': {'mode': 'off',
+                              'prop': 'fit pars: ampli_0'},
                  'list pixels': None,
                  'reverse': False,
                  'del 1st loop': True,
@@ -45,7 +44,7 @@ def example_sort_plot(verbose=False, make_plots=False):
 
     # saving path management
     dir_path_out, save_plots = save_path_example(
-        "sort_plot", save_example_exe=make_plots, save_test_exe=False)
+        "sort_plot_pixel", save_example_exe=make_plots, save_test_exe=False)
     # ex main_sort_plot_pixel
     list_file = main_sort_plot_pixel(
         user_pars, dir_path_in, verbose=verbose, show_plots=make_plots,
@@ -56,4 +55,4 @@ def example_sort_plot(verbose=False, make_plots=False):
 
 if __name__ == '__main__':
     figs = []
-    example_sort_plot(verbose=True, make_plots=True)
+    example_sort_plot_pixel(verbose=True, make_plots=True)
