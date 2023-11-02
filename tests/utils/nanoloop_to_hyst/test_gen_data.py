16ccb519451e98f59bdc4c55be4724b6abf7febe
yudgugger <yudgugger@gmail.com>
2023-11-02 14:26:01 +0100

    rename datas --> data rename loop --> nanoloop rename measurements --> properties adapt many function names change management path for saving file and data in examples checking of global dosctring and comment

--- tests/utils/nanoloop_to_hyst/test_gen_data.py
+++ tests/utils/nanoloop_to_hyst/test_gen_data.py
@@ -1,19 +1,19 @@
 """
-Test gen_datas methods
+Test gen_data methods
 """
 from pytest import approx
 import numpy as np
 
-from examples.utils.nanoloop_to_hyst.ex_gen_data import example_gen_datas
+from examples.utils.nanoloop_to_hyst.ex_gen_data import example_gen_data
 
 
 # class TestGenDatas(unittest.TestCase):
 
 
-def test_gen_datas_multi_off():
-    """ Test gen_datas: off field: multi loop """
+def test_gen_data_multi_off():
+    """ Test gen_data: off field: multi loop """
 
-    datas_dict, dict_str = example_gen_datas(analysis='multi_off')
+    datas_dict, dict_str = example_gen_data(analysis='multi_off')
 
     sum_datas_dict = np.sum(np.concatenate(list(datas_dict.values())))
     # print(sum_datas_dict)
@@ -22,10 +22,10 @@ def test_gen_datas_multi_off():
     assert dict_str == {'unit': 'nm', 'label': 'Off field', 'col': 'w'}
 
 
-def test_gen_datas_mean_off():
-    """ Test gen_datas: off field: mean loop """
+def test_gen_data_mean_off():
+    """ Test gen_data: off field: mean loop """
 
-    datas_dict, dict_str = example_gen_datas(analysis='mean_off')
+    datas_dict, dict_str = example_gen_data(analysis='mean_off')
 
     sum_datas_dict = np.sum(np.concatenate(list(datas_dict.values())))
     # print(sum_datas_dict)
@@ -34,10 +34,10 @@ def test_gen_datas_mean_off():
     assert dict_str == {'unit': 'nm', 'label': 'Off field', 'col': 'w'}
 
 
-def test_gen_datas_mean_on():
-    """ Test gen_datas: on field: mean loop """
+def test_gen_data_mean_on():
+    """ Test gen_data: on field: mean loop """
 
-    datas_dict, dict_str = example_gen_datas(analysis='mean_on')
+    datas_dict, dict_str = example_gen_data(analysis='mean_on')
 
     sum_datas_dict = np.sum(np.concatenate(list(datas_dict.values())))
     # print(sum_datas_dict)
