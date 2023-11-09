930905b70db85a947b1426fd425f3b3759d8d8fe
yudgugger <yudgugger@gmail.com>
2023-11-09 13:54:03 +0100

    New settings management move TomlError and load_parameters_from_file

--- PySSPFM/utils/path_for_runable.py
+++ PySSPFM/utils/path_for_runable.py
@@ -4,83 +4,8 @@ File and directory path management in order to save results from runnable script
 import os
 import shutil
 from datetime import datetime
-import json
 
-from PySSPFM.settings import \
-    SAVE_TEST_EXAMPLE, EXAMPLE_ROOT_PATH_OUT, DEFAULT_DATA_PATH_OUT
-
-
-class TomlError(Exception):
-    """ TomlError object """
-    def __init__(self, message=""):
-        """
-        Object used to generate error when toml files can't be opened
-
-        Parameters
-        ----------
-        message: str, optional
-            Custom error message (default is an empty string).
-        """
-        self.message = message
-        super().__init__(self.message)
-
-
-def load_parameters_from_file(file_path):
-    """
-    Load parameters from a configuration file in either TOML or JSON format.
-
-    Parameters
-    ----------
-    file_path: str
-        Path to the configuration file (in .toml or .json format).
-
-    Returns
-    -------
-    parameters: dict
-        Dictionary containing the loaded parameters.
-    """
-
-    def replace_null_with_none(data):
-        """
-        Recursively replace 'null' with None in a dictionary or list.
-
-        Parameters
-        ----------
-        data : any
-            The data (dict, list, or value) to process.
-
-        Returns
-        -------
-        any
-            The processed data with 'null' replaced by None.
-        """
-        if isinstance(data, dict):
-            for key, value in data.items():
-                data[key] = replace_null_with_none(value)
-        elif isinstance(data, list):
-            for i, item in enumerate(data):
-                data[i] = replace_null_with_none(item)
-        elif data == "null":
-            data = None
-        return data
-
-    _, file_extension = os.path.splitext(file_path)
-    if file_extension == '.toml':
-        try:
-            import toml
-        except (NotImplementedError, NameError) as error:
-            message = "To open toml file , toml module is required"
-            raise TomlError(message) from error
-        with open(file_path, 'r', encoding='utf-8') as toml_file:
-            dict_pars = toml.load(toml_file)
-            dict_pars = replace_null_with_none(dict_pars)
-            return dict_pars
-    elif file_extension == '.json':
-        with open(file_path, 'r', encoding='utf-8') as json_file:
-            return json.load(json_file)
-    else:
-        raise ValueError(
-            "Invalid file format. Supported formats are .toml and .json.")
+from PySSPFM.settings import get_setting
 
 
 def save_path_management(dir_path_in, dir_path_out=None, save=False,
@@ -193,17 +118,17 @@ def save_path_example(folder_name, save_example_exe, save_test_exe=False):
     save_plots: bool
         A flag indicating whether to save plots.
     """
-    if SAVE_TEST_EXAMPLE:
+    if get_setting("save_test_example"):
         if not save_example_exe and not save_test_exe:
             dir_path_out = None
             save_plots = False
         else:
             if save_example_exe:
                 dir_path_out = os.path.join(
-                    EXAMPLE_ROOT_PATH_OUT, f"ex_{folder_name}")
+                    get_setting("example_root_path_out"), f"ex_{folder_name}")
             elif save_test_exe:
                 dir_path_out = os.path.join(
-                    DEFAULT_DATA_PATH_OUT, f"test_{folder_name}")
+                    get_setting("default_data_path_out"), f"test_{folder_name}")
             else:
                 raise IOError("save_example_exe and save_test_exe can't "
                               "be True at the same time")
