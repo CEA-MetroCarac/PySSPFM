0589204ad8c9b363e1d4822234b295dccffc9553
yudgugger <yudgugger@gmail.com>
2023-11-22 16:36:27 +0100

    Adapted data in with changement of default_settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -20,7 +20,7 @@ def test_get_settings_dict_classic():
         "default_nanoloops_folder_name": "nanoloops",
         "default_best_nanoloop_folder_name": "best_nanoloops",
         "default_properties_folder_name": "properties",
-        "save_test_example": true,
+        "save_test_example": True,
         "extract_parameters": "json",
         "key_measurement_extraction": {
             "spm": {
@@ -64,7 +64,7 @@ def test_get_settings_dict_classic():
         "index_line_meas_name": 0,
         "delimiter": "\t\t",
         "figsize": [18, 9],
-        "detect_bug_segments": false,
+        "detect_bug_segments": False,
         "fit_method": "nelder",
         "color_amp_pha_map": "coolwarm",
         "histo_phase_method": "fit",
@@ -72,7 +72,7 @@ def test_get_settings_dict_classic():
         "color_sspfm_map_pixel": "white",
         "color_sspfm_map_highlighted_pixel": "red",
         "color_curve_clustering": "turbo",
-        "electrostatic_offset": true}
+        "electrostatic_offset": True}
 
     for key in target_settings_dict.keys():
         assert settings_dict[key] == target_settings_dict[key]
@@ -129,9 +129,9 @@ def test_get_settings_dict_default():
         "header_lines": 1,
         "index_line_meas_name": 0,
         "delimiter": "\t\t",
-        "detect_bug_segments": false,
+        "detect_bug_segments": False,
         "fit_method": "nelder",
         "histo_phase_method": "fit",
-        "electrostatic_offset": true}
+        "electrostatic_offset": True}
 
     assert def_settings_dict == target_def_settings_dict
