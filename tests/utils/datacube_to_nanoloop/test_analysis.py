6bd1b93bd7aee8e7e658271e3b36c08a7c127702
yudgugger <yudgugger@gmail.com>
2023-11-22 15:22:13 +0100

    Adapat target with Segment changement : Segment --> now splitted in 4 classes: SegmentInfo, SegmentSweep, SegmentStable, SegmentStableDFRT

--- tests/utils/datacube_to_nanoloop/test_analysis.py
+++ tests/utils/datacube_to_nanoloop/test_analysis.py
@@ -14,11 +14,11 @@ from examples.utils.datacube_to_nanoloop.ex_analysis import \
 def test_calib():
     """ Test ex_calib """
 
-    amp_zi_dfrt, pha_zi_dfrt, amp_cal_dfrt = ex_calib()
+    amp_external, pha_external, amp_cal_external = ex_calib()
 
-    assert np.sum(amp_zi_dfrt) == approx(48085791.44687746)
-    assert np.sum(pha_zi_dfrt) == approx(17219838.632536672)
-    assert np.sum(amp_cal_dfrt) == approx(24042895.72343873)
+    assert np.sum(amp_external) == approx(48085791.44687746)
+    assert np.sum(pha_external) == approx(17219838.632536672)
+    assert np.sum(amp_cal_external) == approx(24042895.72343873)
 
 
 def test_init_parameters():
@@ -67,16 +67,11 @@ def test_segments_max_on():
     assert np.sum(seg.amp_tab_init) == approx(11438.773117664969)
     assert seg.bckgnd is None
     assert seg.best_fit == []
-    assert seg.cut_seg == {'start': 10, 'end': 10}
     assert seg.end_ind == 910
-    assert seg.end_ind_init == 920
     assert seg.error == ''
     assert np.sum(seg.freq_tab) == approx(23920.0)
     assert np.sum(seg.freq_tab_init) == approx(29900.0)
-    assert seg.inc_amp is None
-    assert seg.inc_pha is None
     assert seg.len == 80
-    assert seg.len_init == 100
     assert seg.pha == approx(125.71207649637046)
     assert seg.pha_best_fit == []
     assert np.sum(seg.pha_tab) == approx(11973.066691195469)
@@ -84,7 +79,6 @@ def test_segments_max_on():
     assert seg.q_fact == approx(163.0)
     assert seg.res_freq == 326.0
     assert seg.start_ind == 830
-    assert seg.start_ind_init == 820
     assert np.sum(seg.time_tab) == approx(114.00000000000003)
     assert np.sum(seg.time_tab_init) == approx(142.50000000000003)
 
@@ -112,16 +106,11 @@ def test_segments_max_off():
     assert np.sum(seg.amp_tab_init) == approx(9971.142229811065)
     assert seg.bckgnd is None
     assert seg.best_fit == []
-    assert seg.cut_seg == {'start': 10, 'end': 10}
     assert seg.end_ind == 1010
-    assert seg.end_ind_init == 1020
     assert seg.error == ''
     assert np.sum(seg.freq_tab) == approx(23920.0)
     assert np.sum(seg.freq_tab_init) == approx(29900.0)
-    assert seg.inc_amp is None
-    assert seg.inc_pha is None
     assert seg.len == 80
-    assert seg.len_init == 100
     assert seg.pha == approx(226.59664881153964)
     assert seg.pha_best_fit == []
     assert np.sum(seg.pha_tab) == approx(13323.539465541126)
@@ -129,7 +118,6 @@ def test_segments_max_off():
     assert seg.q_fact == approx(74.0)
     assert seg.res_freq == 296.0
     assert seg.start_ind == 930
-    assert seg.start_ind_init == 920
     assert np.sum(seg.time_tab) == approx(118.00000000000003)
     assert np.sum(seg.time_tab_init) == approx(147.50000000000003)
 
@@ -160,16 +148,11 @@ def test_segments_fit_on():
     assert np.sum(seg.amp_tab_init) == approx(11438.773117664969)
     assert seg.bckgnd == approx(12.518852685044568)
     assert np.sum(seg.best_fit) == approx(10881.9123373826)
-    assert seg.cut_seg == {'start': 10, 'end': 10}
     assert seg.end_ind == 910
-    assert seg.end_ind_init == 920
     assert seg.error == ''
     assert np.sum(seg.freq_tab) == approx(23920.0)
     assert np.sum(seg.freq_tab_init) == approx(29900.0)
-    assert seg.inc_amp is None
-    assert seg.inc_pha is None
     assert seg.len == 80
-    assert seg.len_init == 100
     assert seg.pha == approx(179.37831666015322)
     assert np.sum(seg.pha_best_fit) == approx(12037.411509536036)
     assert np.sum(seg.pha_tab) == approx(11973.066691195469)
@@ -177,7 +160,6 @@ def test_segments_fit_on():
     assert seg.q_fact == approx(343.3272390563352, abs=1e-4)
     assert seg.res_freq == approx(326.76409251747384)
     assert seg.start_ind == 830
-    assert seg.start_ind_init == 820
     assert np.sum(seg.time_tab) == approx(114.00000000000003)
     assert np.sum(seg.time_tab_init) == approx(142.50000000000003)
 
@@ -208,16 +190,11 @@ def test_segments_fit_off():
     assert np.sum(seg.amp_tab_init) == approx(9971.142229811065)
     assert seg.bckgnd == approx(1.8890050339911803e-13)
     assert np.sum(seg.best_fit) == approx(9697.147279262597)
-    assert seg.cut_seg == {'start': 10, 'end': 10}
     assert seg.end_ind == 1010
-    assert seg.end_ind_init == 1020
     assert seg.error == ''
     assert np.sum(seg.freq_tab) == approx(23920.0)
     assert np.sum(seg.freq_tab_init) == approx(29900.0)
-    assert seg.inc_amp is None
-    assert seg.inc_pha is None
     assert seg.len == 80
-    assert seg.len_init == 100
     assert seg.pha == approx(183.25126624314055)
     assert np.sum(seg.pha_best_fit) == approx(13205.780336868818)
     assert np.sum(seg.pha_tab) == approx(13323.539465541126)
@@ -225,7 +202,6 @@ def test_segments_fit_off():
     assert seg.q_fact == approx(341.7160661770409, abs=1e-4)
     assert seg.res_freq == approx(295.0595059372857)
     assert seg.start_ind == 930
-    assert seg.start_ind_init == 920
     assert np.sum(seg.time_tab) == approx(118.00000000000003)
     assert np.sum(seg.time_tab_init) == approx(147.50000000000003)
 
@@ -251,26 +227,16 @@ def test_segments_dfrt_on():
     assert seg.amp == approx(30.05119398728839)
     assert np.sum(seg.amp_tab) == approx(2404.095518983071)
     assert np.sum(seg.amp_tab_init) == approx(2974.1984182330775)
-    assert seg.bckgnd is None
-    assert seg.best_fit == []
-    assert seg.cut_seg == {'start': 10, 'end': 10}
     assert seg.end_ind == 910
-    assert seg.end_ind_init == 920
-    assert seg.error == ''
-    assert np.sum(seg.freq_tab) == approx(23920.0)
-    assert np.sum(seg.freq_tab_init) == approx(29900.0)
+    assert np.sum(seg.freq_tab) is None
+    assert np.sum(seg.freq_tab_init) is None
     assert seg.inc_amp == approx(0.6291232603935817)
     assert seg.inc_pha == approx(2.0731919158003196)
     assert seg.len == 80
-    assert seg.len_init == 100
     assert seg.pha == approx(90.9675711197639)
-    assert seg.pha_best_fit == []
     assert np.sum(seg.pha_tab) == approx(7277.405689581113)
     assert np.sum(seg.pha_tab_init) == approx(8996.054642657837)
-    assert seg.q_fact is None
-    assert seg.res_freq is None
     assert seg.start_ind == 830
-    assert seg.start_ind_init == 820
     assert np.sum(seg.time_tab) == approx(114.00000000000003)
     assert np.sum(seg.time_tab_init) == approx(142.50000000000003)
 
@@ -296,25 +262,15 @@ def test_segments_dfrt_off():
     assert seg.amp == approx(36.87806856414231)
     assert np.sum(seg.amp_tab) == approx(2950.245485131385)
     assert np.sum(seg.amp_tab_init) == approx(3655.151450529881)
-    assert seg.bckgnd is None
-    assert seg.best_fit == []
-    assert seg.cut_seg == {'start': 10, 'end': 10}
     assert seg.end_ind == 1010
-    assert seg.end_ind_init == 1020
-    assert seg.error == ''
-    assert np.sum(seg.freq_tab) == approx(23920.0)
-    assert np.sum(seg.freq_tab_init) == approx(29900.0)
+    assert np.sum(seg.freq_tab) is None
+    assert np.sum(seg.freq_tab_init) is None
     assert seg.inc_amp == approx(0.6835243969792533)
     assert seg.inc_pha == approx(1.5333272675407086)
     assert seg.len == 80
-    assert seg.len_init == 100
     assert seg.pha == approx(72.0800326320837)
-    assert seg.pha_best_fit == []
     assert np.sum(seg.pha_tab) == approx(5766.402610566696)
     assert np.sum(seg.pha_tab_init) == approx(7132.943058414065)
-    assert seg.q_fact is None
-    assert seg.res_freq is None
     assert seg.start_ind == 930
-    assert seg.start_ind_init == 920
     assert np.sum(seg.time_tab) == approx(118.00000000000003)
     assert np.sum(seg.time_tab_init) == approx(147.50000000000003)
