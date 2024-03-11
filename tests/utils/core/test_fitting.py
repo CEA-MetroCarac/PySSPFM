4d8f04ac437ed4b88f199d97ff6200cf8dede2bb
yudgugger <yudgugger@gmail.com>
2024-03-11 11:20:59 +0100

    Dapat target to fit method (default) changement --> least_square (before nelder)

--- tests/utils/core/test_fitting.py
+++ tests/utils/core/test_fitting.py
@@ -12,6 +12,8 @@ def test_gaussian_peak_fit():
     """ test ex_gaussian_peak_fit """
     gaussian_pars = ex_gaussian_peak_fit()
     gaussian_pars_val = [pars.value for pars in gaussian_pars.values()]
+    # for elem in gaussian_pars_val:
+    #     print(elem)
     target_pars = [10.263530347730656, 3.955672070340832, 0.0233435218625182,
                    1.9976776975995745, 0.0]
     for elem_pars, elem_target in zip(gaussian_pars_val, target_pars):
@@ -22,6 +24,8 @@ def test_sho_peak_fit():
     """ test ex_sho_peak_fit """
     sho_pars = ex_sho_peak_fit()
     sho_pars_val = [pars.value for pars in sho_pars.values()]
+    # for elem in sho_pars_val:
+    #     print(elem)
     target_pars = [1.0612698195104044, 9.568395446904102, 9.998240445405514,
                    1.9359077592571658, 0.0]
     for elem_pars, elem_target in zip(sho_pars_val, target_pars):
@@ -32,6 +36,8 @@ def test_sho_phase_fit():
     """ test ex_sho_phase_fit """
     sho_phase_pars = ex_sho_phase_fit()
     sho_phase_pars_val = [pars.value for pars in sho_phase_pars.values()]
+    # for elem in sho_phase_pars_val:
+    #     print(elem)
     target_pars = [0.9985985077988063, 10.215378931180616, 9.967515484523119,
                    1.0041064481679576, 0.0]
     for elem_pars, elem_target in zip(sho_phase_pars_val, target_pars):
@@ -43,6 +49,8 @@ def test_sho_phase_switch_fit():
     sho_phase_switch_pars = ex_sho_phase_switch_fit()
     sho_phase_switch_pars_val = [pars.value
                                  for pars in sho_phase_switch_pars.values()]
+    # for elem in sho_phase_switch_pars_val:
+    #     print(elem)
     target_pars = [1.0976692783807431, 2.3684479138424592, 10.000000120218424,
                    1.0039096300857406, 0.0]
     for elem_pars, elem_target in zip(sho_phase_switch_pars_val, target_pars):
