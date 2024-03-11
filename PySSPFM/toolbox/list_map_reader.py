ecfbbaf57af02a9fa0c2491af3791c48a8248ff4
yudgugger <yudgugger@gmail.com>
2024-03-11 14:21:51 +0100

    Add graphs of the evolution of parameters measured over time are displayed (function treatment_plot_graph, formatting_fig_graphs), addition of the parameter meas time which specifies the total duration of the measurement

--- PySSPFM/toolbox/list_map_reader.py
+++ PySSPFM/toolbox/list_map_reader.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from PySSPFM.settings import get_setting
 from PySSPFM.utils.core.extract_params_from_file import \
     load_parameters_from_file
-from PySSPFM.utils.core.figure import print_plots
+from PySSPFM.utils.core.figure import print_plots, plot_graph
 from PySSPFM.utils.nanoloop.plot import subplots_dim
 from PySSPFM.utils.nanoloop_to_hyst.file import extract_properties
 from PySSPFM.utils.map.plot import disable_ax, final_map, intermediate_map
@@ -101,47 +101,109 @@ def main_list_map_reader(user_pars, dir_path_in, verbose=False):
             print('# reference property')
         fig_ref = plt.figure(figsize=figsize)
         fig_ref.sfn = "list_map_reader_ref"
-        axs_ref = formatting_fig(fig_ref, [1, 1], applied_mask, nb_map=1,
-                                 dict_map=None, dict_ref=None, dim_mic=None)
+        axs_ref = formatting_fig_maps(
+            fig_ref, [1, 1], applied_mask, nb_map=1, dict_map=None,
+            dict_ref=None, dim_mic=None)
         ref_str = f'ref prop: {user_pars["ref"]["prop"]}' \
                   f' ({user_pars["ref"]["mode"]})'
-        tratment_plot_map(
+        treatment_plot_map(
             fig_ref, axs_ref[0], ref, dim_pix, dim_mic=dim_mic,
             dict_interp=dict_interp, mask=applied_mask, prop_str=ref_str,
             plot_ind=True)
         fig_ref.tight_layout()
         maps += [fig_ref]
 
-    # Plot multi_prop
+    # Plot multi_prop map
     if verbose:
         print('# multi mapping')
     nb_map = len(list(multi_prop.keys()))
     fig_maps_dim = subplots_dim(nb_map)
     fig_maps = plt.figure(figsize=figsize)
     fig_maps.sfn = "list_map_reader_maps"
-    axs_maps = formatting_fig(
+    axs_maps = formatting_fig_maps(
         fig_maps, fig_maps_dim, applied_mask,  nb_map=nb_map, dict_map=None,
         dict_ref=None, dim_mic=None)
-    # Generate a map for each properties
+    # Plot multi_prop graph
+    fig_graphs = plt.figure(figsize=figsize)
+    fig_graphs.sfn = "list_map_reader_graphs"
+    axs_graphs = formatting_fig_graphs(fig_graphs, fig_maps_dim,
+                                       nb_graph=nb_map)
+
+    # Generate a map and graph for each property
     plot_ind = len(axs_maps) <= 10
     for i, (lab_prop, prop) in enumerate(multi_prop.items()):
         if verbose:
             print(f'\t- {lab_prop}')
         # Treat and plot map: property
-        tratment_plot_map(
+        treatment_plot_map(
             fig_maps, axs_maps[i], prop, dim_pix, dim_mic=dim_mic,
             dict_interp=dict_interp, mask=applied_mask, prop_str=lab_prop,
             plot_ind=plot_ind)
+
+        # Treat and plot graph: property
+        treatment_plot_graph(axs_graphs[i], prop, nb_line=dim_pix['y'],
+                             mask=applied_mask, prop_str=lab_prop,
+                             meas_time=user_pars["meas time"])
+
     fig_maps.tight_layout()
+    fig_graphs.tight_layout()
     maps += [fig_maps]
+    maps += [fig_graphs]
     figures = corr_table + maps
 
     return figures
 
 
-def tratment_plot_map(fig, ax, propertie, dim_pix, dim_mic=None,
-                      dict_interp=None, mask=None, prop_str=None,
-                      plot_ind=False):
+def treatment_plot_graph(ax_graph, prop, nb_line, mask=None, prop_str=None,
+                         meas_time=None):
+    """
+    Plot graph for treatment
+
+    Parameters
+    ----------
+    ax_graph: plt.Axes
+        Axis for plotting
+    prop: list
+        Property values
+    nb_line: int
+        Number of lines of the map
+    mask: list, optional
+        List of indexes to mask
+    prop_str: str, optional
+        Property string
+    meas_time: float, optional
+        Total measurement time
+
+    Returns
+    -------
+    None
+    """
+    prop_str = "" if prop_str is None else prop_str
+    mask = [] if mask is None else mask
+    line_indexs = np.linspace(0, nb_line, len(prop))
+    if meas_time is not None:
+        time_tab = np.linspace(0, meas_time, len(prop))
+        time_tab = [value for index, value in enumerate(time_tab)
+                    if index not in mask]
+    line_indexs = [value for index, value in enumerate(line_indexs)
+                   if index not in mask]
+    prop = [value for index, value in enumerate(prop) if index not in mask]
+    plot_dict = {'x lab': 'Line index', 'y lab': f'{prop_str}', 'fs': 15,
+                 'edgew': 1, 'tickl': 2, 'gridw': 1}
+    tab_dict = {'form': 'g-'}
+    plot_graph(ax_graph, line_indexs, prop, plot_dict=plot_dict,
+               tabs_dict=tab_dict, plot_leg=False)
+    if meas_time is not None:
+        ax2 = ax_graph.twiny()
+        ax2.set_xlabel('Time (h)', fontsize=plot_dict['fs'])
+        ax2.plot(time_tab, prop, alpha=0)
+        ax2.tick_params(axis='both', which='major', length=plot_dict['tickl'],
+                        labelsize=plot_dict['fs'])
+
+
+def treatment_plot_map(fig, ax, propertie, dim_pix, dim_mic=None,
+                       dict_interp=None, mask=None, prop_str=None,
+                       plot_ind=False):
     """
     Treat property map (interpolation, mask ...) and plot it
 
@@ -192,10 +254,10 @@ def tratment_plot_map(fig, ax, propertie, dim_pix, dim_mic=None,
             index_blank=index_blank, ax_title=prop_str, highlight_pix=None)
 
 
-def formatting_fig(fig, fig_dim, mask, nb_map=None, dict_map=None,
-                   dict_ref=None, dim_mic=None):
+def formatting_fig_maps(fig, fig_dim, mask, nb_map=None, dict_map=None,
+                        dict_ref=None, dim_mic=None):
     """
-    Formatting figure
+    Formatting map figure
 
     Parameters
     ----------
@@ -265,6 +327,35 @@ def formatting_fig(fig, fig_dim, mask, nb_map=None, dict_map=None,
     return axs
 
 
+def formatting_fig_graphs(fig, fig_dim, nb_graph=None):
+    """
+    Formatting graph figure
+
+    Parameters
+    ----------
+    fig: plt.figure
+        Figure object
+    fig_dim: list(2) of int
+        List of figure dimension [nb of line, mb of column]
+    nb_graph: int, optional
+        Number of plotted graph
+
+    Returns
+    -------
+    axs: list(nb_graph) of plt.axes
+        Axes of the figure
+    """
+    # Ax management
+    main_ax = fig.add_subplot(111)
+    axs = [main_ax] if int(fig_dim[0] * fig_dim[1]) == 1 else []
+    if int(fig_dim[0] * fig_dim[1]) > 1:
+        disable_ax(main_ax)
+        axs = [fig.add_subplot(fig_dim[0], fig_dim[1], i + 1, sharex=main_ax)
+               for i in range(int(fig_dim[0] * fig_dim[1])) if i < nb_graph]
+
+    return axs
+
+
 def parameters():
     """
     To complete by user of the script: return parameters for analysis
@@ -287,6 +378,12 @@ def parameters():
         This parameter specifies the interpolation function used for sspfm
         maps interpolation. It can take one of the following values:
         'linear', or 'cubic'.
+    - meas_time: float
+        Real duration of the measurement in hours.
+        This parameter represents the actual duration of the measurement in
+        hours. It is used to generate a time axis for the property graphs
+        corresponding to the measurements. If set to None, no time axis is
+        generated, and only the line index is used as the x-axis values.
     - revert_mask: bool
         Revert option of the mask for selecting specific files.
         This parameter specifies if the mask should be reverted (True), or not
@@ -367,6 +464,7 @@ def parameters():
         user_pars = {'ind maps': ind_maps,
                      'interp fact': 3,
                      'interp func': 'linear',
+                     'meas time': None,
                      'revert mask': False,
                      'man mask': [],
                      'ref': {'mode': 'off',
