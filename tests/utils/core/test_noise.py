2a3176ff629b83fc4a5c3286e9b651e44b46d03e
yudgugger <yudgugger@gmail.com>
2024-03-11 15:48:20 +0100

    Add precision on filter

--- tests/utils/core/test_noise.py
+++ tests/utils/core/test_noise.py
@@ -52,7 +52,7 @@ def test_ex_butter_filter_bandpass():
 
     y_filt_bandpass = ex_butter_filter(filter_type='bandpass')
 
-    assert np.sum(y_filt_bandpass) == approx(-24.70597466183571)
+    assert np.sum(y_filt_bandpass) == approx(-24.70597466183571, abs=1e-5)
 
 
 def test_ex_butter_filter_bandstop():
@@ -60,4 +60,4 @@ def test_ex_butter_filter_bandstop():
 
     y_filt_bandstop = ex_butter_filter(filter_type='bandstop')
 
-    assert np.sum(y_filt_bandstop) == approx(33.83620308961862)
+    assert np.sum(y_filt_bandstop) == approx(33.83620308961862, abs=1e-5)
