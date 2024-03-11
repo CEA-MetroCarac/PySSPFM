bb7406340e2bd14076bf4ef1a636fa3c31329152
yudgugger <yudgugger@gmail.com>
2024-03-11 13:05:31 +0100

    Generate file paths from nanoloops and also raw measurements MAJ targets (with changement of measurement sheet)

--- tests/utils/nanoloop_to_hyst/test_file.py
+++ tests/utils/nanoloop_to_hyst/test_file.py
@@ -1,6 +1,7 @@
 """
 Test file methods
 """
+import os
 import numpy as np
 
 from examples.utils.nanoloop_to_hyst.ex_file import example_file
@@ -13,15 +14,16 @@ def test_file():
     """ Test example_file """
 
     out = example_file()
-    (file_paths, meas_pars, sign_pars, dict_analysis_1, nb_write_per_read,
-     write_segment, properties, dim_pix, dim_mic) = out
+    (file_paths_from_nanoloops, file_paths_from_raw, meas_pars, sign_pars,
+     dict_analysis_1, nb_write_per_read, write_segment, properties, dim_pix,
+     dim_mic) = out
 
     assert nb_write_per_read == 100
     assert len(list(write_segment)) == 100
     assert len(dict_analysis_1) == 3
-    assert len(list(file_paths)) == 63
-    assert len(meas_pars) == 33
-    assert len(sign_pars) == 12
+    assert file_names_from_nanoloops == file_names_from_raw
+    assert len(meas_pars) == 31
+    assert len(sign_pars) == 16
     assert np.sum(list(dim_pix.values())) == 16
     assert np.sum(list(dim_mic.values())) == 7
     assert np.sum(list(properties['on'].values())) == 6269
