0966c6ea696ef3615e71712376302e3be0dcfde6
yudgugger <yudgugger@gmail.com>
2024-03-11 13:37:51 +0100

    change target due to changement of hysteresis sens of rotation

--- tests/toolbox/test_curve_clustering.py
+++ tests/toolbox/test_curve_clustering.py
@@ -31,113 +31,66 @@ def test_curve_clustering_piezoresponse():
     # print(np.sum(avg_curve["coupled"][2]))
     # print(np.sum(avg_curve["coupled"][3]))
 
-    indexs_off = [3, 1, 3, 3, 3, 4, 1, 3, 2, 2, 2, 1, 3, 3, 3, 3, 2, 2, 2,
-                  2, 0, 4, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                  1, 1, 2, 3, 3, 3]
+    indexs_off = [3, 1, 3, 3, 3, 4, 1, 3, 2, 2, 2, 1, 3, 3, 3, 3, 2, 2, 2, 2,
+                  4, 4, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0,
+                  0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2,
+                  3, 3, 3]
     indexs_on = [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                  0, 0, 0]
     indexs_coupled = [2, 3, 2, 2, 2, 1, 3, 2, 1, 1, 1, 3, 2, 2, 1, 1, 0, 1,
                       1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      0, 0, 0, 0, 0, 0, 2, 1, 1]
+                      0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 2, 2, 1]
 
     assert cluster_indexs["off"] == indexs_off
     assert cluster_indexs["on"] == indexs_on
     assert cluster_indexs["coupled"] == indexs_coupled
 
-    assert inertia["off"] == approx(0.139395115757726)
-    assert inertia["on"] == approx(0.28498500984901537)
-    assert inertia["coupled"] == approx(0.1642794418242845)
+    assert inertia["off"] == approx(0.13523678975557835)
+    assert inertia["on"] == approx(0.28663984068106296)
+    assert inertia["coupled"] == approx(0.1620727015123082)
 
-    assert np.sum(avg_curve["off"][0]) == approx(0.5936781031803703)
-    assert np.sum(avg_curve["off"][1]) == approx(0.2831871919714286)
-    assert np.sum(avg_curve["off"][2]) == approx(0.8627956580466666)
-    assert np.sum(avg_curve["off"][3]) == approx(0.15602010488333334)
-    assert np.sum(avg_curve["off"][4]) == approx(2.3118014035)
+    assert np.sum(avg_curve["off"][0]) == approx(-0.5604060286538461)
+    assert np.sum(avg_curve["off"][1]) == approx(-0.32359891999833335)
+    assert np.sum(avg_curve["off"][2]) == approx(-0.8637471184000002)
+    assert np.sum(avg_curve["off"][3]) == approx(-0.15399308193333336)
+    assert np.sum(avg_curve["off"][4]) == approx(-2.242400310125)
 
-    assert np.sum(avg_curve["on"][0]) == approx(0.45226276894237294)
-    assert np.sum(avg_curve["on"][1]) == approx(0.5239598472500001)
+    assert np.sum(avg_curve["on"][0]) == approx(-0.4599520117610169)
+    assert np.sum(avg_curve["on"][1]) == approx(-0.434709211)
 
     assert np.sum(avg_curve["coupled"][0]) == approx(
-        0.448606402302103)
+        -0.3895835032539335)
     assert np.sum(avg_curve["coupled"][1]) == approx(
-        0.6988583802447283)
+        -0.7495477722962939)
     assert np.sum(avg_curve["coupled"][2]) == approx(
-        -0.36790395567739304)
-    assert np.sum(avg_curve["coupled"][3]) == approx(0.5236255422617767)
+        0.2412386496437579)
+    assert np.sum(avg_curve["coupled"][3]) == approx(-0.39787115575800425)
 
-    assert cluster_info["off"][0][0] == approx(0.0)
-    assert cluster_info["off"][1][0] == approx(0.07786968990413191)
-    assert cluster_info["off"][2][0] == approx(0.09866019954139532)
-    assert cluster_info["off"][3][0] == approx(0.1802770711211079)
-    assert cluster_info["off"][4][0] == approx(0.2146512578110123)
+    assert cluster_info["off"][0] == [0.0, 0.07695897085508642, 'B', 26, 'A']
+    assert cluster_info["off"][1] == [0.07695897085508642, 0.07695897085508642,
+                                      'A', 6, 'B']
+    assert cluster_info["off"][2] == [0.0991076879668917, 0.09910768796689168,
+                                      'A', 15, 'C']
+    assert cluster_info["off"][3] == [0.17859580758813837, 0.11473940080561688,
+                                      'C', 12, 'D']
+    assert cluster_info["off"][4] == [0.1911772239978829, 0.14257219838645985,
+                                      'C', 4, 'E']
 
-    assert cluster_info["on"][0][0] == approx(0.0)
-    assert cluster_info["on"][0][1] == approx(0.2519488588987771)
+    assert cluster_info["on"][0] == [0.0, 0.25579396755296596, 'B', 59, 'A']
+    assert cluster_info["on"][1] == [0.25579396755296596, 0.25579396755296596,
+                                     'A', 4, 'B']
 
-    assert cluster_info["coupled"][0][0] == approx(0.0)
-    assert cluster_info["coupled"][1][0] == approx(0.09978083565264455)
-    assert cluster_info["coupled"][2][0] == approx(0.17830418305190662)
-    assert cluster_info["coupled"][3][0] == approx(0.3133612845140547)
-
-    assert cluster_info["off"][0][1] == approx(0.07786968990413191)
-    assert cluster_info["off"][1][1] == approx(0.07786968990413191)
-    assert cluster_info["off"][2][1] == approx(0.09866019954139532)
-    assert cluster_info["off"][3][1] == approx(0.11514522098772867)
-    assert cluster_info["off"][4][1] == approx(0.15693006953830294)
-
-    assert cluster_info["on"][0][1] == approx(0.2519488588987771)
-    assert cluster_info["on"][1][1] == approx(0.25194885889877716)
-
-    assert cluster_info["coupled"][0][1] == approx(0.09978083565264455)
-    assert cluster_info["coupled"][1][1] == approx(0.09978083565264455)
-    assert cluster_info["coupled"][2][1] == approx(0.13008345719926323)
-    assert cluster_info["coupled"][3][1] == approx(0.3133612845140547)
-
-    assert cluster_info["off"][0][2] == "B"
-    assert cluster_info["off"][1][2] == "A"
-    assert cluster_info["off"][2][2] == "A"
-    assert cluster_info["off"][3][2] == "C"
-    assert cluster_info["off"][4][2] == "C"
-
-    assert cluster_info["on"][0][2] == "B"
-    assert cluster_info["on"][1][2] == "A"
-
-    assert cluster_info["coupled"][0][2] == "B"
-    assert cluster_info["coupled"][1][2] == "A"
-    assert cluster_info["coupled"][2][2] == "B"
-    assert cluster_info["coupled"][3][2] == "A"
-
-    assert cluster_info["off"][0][3] == 27
-    assert cluster_info["off"][1][3] == 7
-    assert cluster_info["off"][2][3] == 15
-    assert cluster_info["off"][3][3] == 12
-    assert cluster_info["off"][4][3] == 2
-
-    assert cluster_info["on"][0][3] == 59
-    assert cluster_info["on"][1][3] == 4
-
-    assert cluster_info["coupled"][0][3] == 34
-    assert cluster_info["coupled"][1][3] == 18
-    assert cluster_info["coupled"][2][3] == 8
-    assert cluster_info["coupled"][3][3] == 3
-
-    assert cluster_info["off"][0][4] == "A"
-    assert cluster_info["off"][1][4] == "B"
-    assert cluster_info["off"][2][4] == "C"
-    assert cluster_info["off"][3][4] == "D"
-    assert cluster_info["off"][4][4] == "E"
-
-    assert cluster_info["on"][0][4] == "A"
-    assert cluster_info["on"][1][4] == "B"
-
-    assert cluster_info["coupled"][0][4] == "A"
-    assert cluster_info["coupled"][1][4] == "B"
-    assert cluster_info["coupled"][2][4] == "C"
-    assert cluster_info["coupled"][3][4] == "D"
+    assert cluster_info["coupled"][0] == \
+           [0.0, 0.09424982911577919, 'B', 33, 'A']
+    assert cluster_info["coupled"][1] == \
+           [0.09424982911577919, 0.09424982911577919, 'A', 18, 'B']
+    assert cluster_info["coupled"][2] == \
+           [0.169070781895064, 0.12835058633199692, 'B', 9, 'C']
+    assert cluster_info["coupled"][3] == \
+           [0.31060770971176105, 0.31060770971176105, 'A', 3, 'D']
 
 
 def test_curve_clustering_amp_pha():
@@ -159,102 +112,36 @@ def test_curve_clustering_amp_pha():
                  1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                  0, 0, 0]
+    indexs_off = [3, 2, 3, 3, 3, 4, 2, 3, 1, 1, 1, 2, 3, 3, 3, 3, 1, 1, 1, 1,
+                  4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+                  0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 3,
+                  3, 3, 3]
 
-    # Get Python version
-    version_info = sys.version_info
-    python_version = f"{version_info.major}.{version_info.minor}"
-
-    # Target adapted depending on Python version
-    if python_version in ["3.8", "3.9", "3.10"] or version_info.minor >= 8:
-        indexs_off = [3, 1, 3, 3, 3, 4, 1, 3, 3, 3, 2, 1, 3, 3, 3, 3, 3, 2, 2,
-                      2, 4, 4, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                      1, 1, 3, 3, 3, 3]
-        inertia_off = 96.59258253235751
-        avg_curve_off_1 = 69.47246188061166
-        avg_curve_off_2 = 83.07405484256466
-        avg_curve_off_3 = 91.95712529778717
-        cluster_info_off_1_0 = 2.1301929582074703
-        cluster_info_off_2_0 = 2.1743304237987306
-        cluster_info_off_3_0 = 3.342860315025872
-        cluster_info_off_3_1 = 2.29989794039676
-        cluster_info_off_4_1 = 4.18485558614015
-        cluster_info_off_1_3 = 6
-        cluster_info_off_2_3 = 12
-        cluster_info_off_3_3 = 16
-    else:
-        indexs_off = [3, 1, 3, 3, 3, 4, 1, 3, 2, 2, 2, 1, 3, 3, 3, 3, 2, 2, 2,
-                      2, 4, 4, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                      2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                      1, 1, 3, 3, 3, 3]
-        inertia_off = 94.9635356607568
-        avg_curve_off_1 = 72.50786208611287
-        avg_curve_off_2 = 82.10108346249717
-        avg_curve_off_3 = 94.46671008317617
-        cluster_info_off_1_0 = 2.1113160018913417
-        cluster_info_off_2_0 = 2.254872025351866
-        cluster_info_off_3_0 = 3.5300633820041
-        cluster_info_off_3_1 = 2.2849739792580923
-        cluster_info_off_4_1 = 4.22877631793445
-        cluster_info_off_1_3 = 7
-        cluster_info_off_2_3 = 14
-        cluster_info_off_3_3 = 13
     assert cluster_indexs["off"] == indexs_off
     assert cluster_indexs["on"] == indexs_on
 
-    assert inertia["off"] == approx(inertia_off)
-    assert inertia["on"] == approx(85.9572013452021)
+    assert inertia["off"] == approx(111.13578587730699)
+    assert inertia["on"] == approx(91.94596697427032)
 
-    assert np.sum(avg_curve["off"][0]) == approx(66.12804145794577)
-    assert np.sum(avg_curve["off"][1]) == approx(avg_curve_off_1)
-    assert np.sum(avg_curve["off"][2]) == approx(avg_curve_off_2)
-    assert np.sum(avg_curve["off"][3]) == approx(avg_curve_off_3)
-    assert np.sum(avg_curve["off"][4]) == approx(72.96209700766119)
+    assert np.sum(avg_curve["off"][0]) == approx(67.44480269778579)
+    assert np.sum(avg_curve["off"][1]) == approx(85.049189857557)
+    assert np.sum(avg_curve["off"][2]) == approx(47.21474992974796)
+    assert np.sum(avg_curve["off"][3]) == approx(98.4523948723894)
+    assert np.sum(avg_curve["off"][4]) == approx(73.05500191702258)
 
-    assert np.sum(avg_curve["on"][0]) == approx(108.58805972420119)
-    assert np.sum(avg_curve["on"][1]) == approx(86.93195273947686)
+    assert np.sum(avg_curve["on"][0]) == approx(114.13229998089432)
+    assert np.sum(avg_curve["on"][1]) == approx(87.0701917281026)
 
-    assert cluster_info["off"][0][0] == approx(0.0)
-    assert cluster_info["off"][1][0] == approx(cluster_info_off_1_0)
-    assert cluster_info["off"][2][0] == approx(cluster_info_off_2_0)
-    assert cluster_info["off"][3][0] == approx(cluster_info_off_3_0)
-    assert cluster_info["off"][4][0] == approx(5.1303263806638535)
+    assert cluster_info["off"][0] == [0.0, 2.436570085411072, 'B', 26, 'A']
+    assert cluster_info["off"][1] == \
+           [2.436570085411072, 2.436570085411072, 'A', 14, 'B']
+    assert cluster_info["off"][2] == \
+           [3.1368359442253464, 3.1368359442253464, 'A', 6, 'C']
+    assert cluster_info["off"][3] == \
+           [3.8185458951727846, 2.4426456403161194, 'B', 13, 'D']
+    assert cluster_info["off"][4] == \
+           [4.861100035372178, 3.620470997547831, 'C', 4, 'E']
 
-    assert cluster_info["on"][0][0] == approx(0.0)
-    assert cluster_info["on"][0][1] == approx(2.732333357603488)
-
-    assert cluster_info["off"][0][1] == approx(cluster_info_off_1_0)
-    assert cluster_info["off"][1][1] == approx(cluster_info_off_1_0)
-    assert cluster_info["off"][2][1] == approx(cluster_info_off_2_0)
-    assert cluster_info["off"][3][1] == approx(cluster_info_off_3_1)
-    assert cluster_info["off"][4][1] == approx(cluster_info_off_4_1)
-
-    assert cluster_info["on"][0][1] == approx(2.732333357603488)
-    assert cluster_info["on"][1][1] == approx(2.732333357603488)
-
-    assert cluster_info["off"][0][2] == "B"
-    assert cluster_info["off"][1][2] == "A"
-    assert cluster_info["off"][2][2] == "A"
-    assert cluster_info["off"][3][2] == "C"
-    assert cluster_info["off"][4][2] == "C"
-
-    assert cluster_info["on"][0][2] == "B"
-    assert cluster_info["on"][1][2] == "A"
-
-    assert cluster_info["off"][0][3] == 26
-    assert cluster_info["off"][1][3] == cluster_info_off_1_3
-    assert cluster_info["off"][2][3] == cluster_info_off_2_3
-    assert cluster_info["off"][3][3] == cluster_info_off_3_3
-    assert cluster_info["off"][4][3] == 3
-
-    assert cluster_info["on"][0][3] == 56
-    assert cluster_info["on"][1][3] == 7
-
-    assert cluster_info["off"][0][4] == "A"
-    assert cluster_info["off"][1][4] == "B"
-    assert cluster_info["off"][2][4] == "C"
-    assert cluster_info["off"][3][4] == "D"
-    assert cluster_info["off"][4][4] == "E"
-
-    assert cluster_info["on"][0][4] == "A"
-    assert cluster_info["on"][1][4] == "B"
+    assert cluster_info["on"][0] == [0.0, 2.9493831147512637, 'B', 56, 'A']
+    assert cluster_info["on"][1] == \
+           [2.9493831147512637, 2.9493831147512637, 'A', 7, 'B']
