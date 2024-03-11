f2aa62e52018180ca4db2b698f2e6fa600f9d875
yudgugger <yudgugger@gmail.com>
2024-03-11 13:21:12 +0100

    test for phase_offset_analyzer.py: used to calculate the phase shift of a series of measurement files

---
+++ tests/toolbox/test_phase_offset_analyzer.py
@@ -0,0 +1,30 @@
+"""
+Test phase_offset_analyzer methods
+"""
+from pytest import approx, skip
+import numpy as np
+
+from examples.toolbox.ex_phase_offset_analyzer import \
+    example_phase_offset_analyzer
+from PySSPFM.utils.raw_extraction import NanoscopeError
+
+
+# class TestCorrelation(unittest.TestCase):
+
+
+def test_phase_offset_analyzer():
+    """ Test phase_offset_analyzer """
+    try:
+        phase_offset_tab = example_phase_offset_analyzer()
+    except NanoscopeError:
+        skip("Test skipped (NanoscopeError): spm file can't be opened without "
+             "Nanoscope Analysis DLL")
+
+    # print(np.nansum(phase_offset_tab['On field']))
+    # print(np.nansum(phase_offset_tab['Off field']))
+    # print(np.nansum(phase_offset_tab['Mean']))
+
+    assert np.nansum(phase_offset_tab['On field']) == approx(1644.3122963182652)
+    assert np.nansum(phase_offset_tab['Off field']) == \
+           approx(1677.1248775829279)
+    assert np.nansum(phase_offset_tab['Mean']) == approx(1750.3109771168647)
