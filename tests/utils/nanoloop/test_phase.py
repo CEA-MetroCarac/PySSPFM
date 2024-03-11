aea5c27500e43173e61ad4595595904c715e9980
yudgugger <yudgugger@gmail.com>
2024-03-11 14:06:11 +0100

    - Unipolar Case of unipolar nanoloops: Consideration in the phase_calibration function (unipolar_revert_analysis Added a new setting "unipolar_phase_revert" to determine if the phase is reversed in the case of unipolar nanoloop
    - Phase offset
    Add an optional offset phase applicable on all initial phase values: allows to correct unwanted phase switches that noise nanoloops
    mean_phase_offset to calculate the mean phase offset of an sspfm file from its phase offset dictionary containing the values in Off Field and On Field
    phase_offset_determination allows to calculate the phase offset from a phase list, by referencing the two phase histogram peaks
    refocused_phase_histo allows you to calculate the values of the recentred histogram and draw it
    apply_phase_offset allows to apply a post measurement phase offset respecting the range of available phase values
    
    - Other Major
    Separation of the plot from the rest of the analysis, in a new python plot_phase file
    Fixed bug on allocation of peak phase value
    Decomposition according to phase_calibration which made too big block (gen_dict_pha, theoretical_phase_analysis, phase_bias_grad, unipolar_revert_analysis, bipolar_revert_analysis
    Separation of the plot from the analysis by doing a new plot_main_peaks function outside find_main_peaks
    
    - Other Minor
    Minor corrections in comments

--- tests/utils/nanoloop/test_phase.py
+++ tests/utils/nanoloop/test_phase.py
@@ -2,16 +2,58 @@
 Test phase methods
 """
 from pytest import approx
+import numpy as np
 
-from examples.utils.nanoloop.ex_phase import ex_phase_calibration
+from examples.utils.nanoloop.ex_phase import \
+    (ex_exp_phase_calibration, ex_simulated_phase_calibration,
+     ex_phase_offset_determination, ex_apply_phase_offset)
 
 
 # class TestPhase(unittest.TestCase):
 
-def test_phase_up_down():
-    """ Test ex_phase_calibration: up and down """
+def test_bipolar_phase():
+    """ Test ex_exp_phase_calibration: bipolar phase nanoloop """
 
-    treat_phase, result, dict_pha = ex_phase_calibration(pha_corr='up_down')
+    treat_phase, result = \
+        ex_exp_phase_calibration(type_of_data='bipolar', pha_corr='offset')
+
+    # print(sum(treat_phase))
+
+    assert result['corr'] == 'offset'
+    assert result['reverse'] is True
+    assert result['coefs'][0] == -1
+    assert result['coefs'][1] == approx(127.57339999999999)
+    assert result['dict phase target']['low'] == 0
+    assert result['dict phase target']['high'] == 180
+    assert result['dict phase meas']['low'] == approx(126.49859999999998)
+    assert result['dict phase meas']['high'] == approx(-51.3518)
+    assert sum(treat_phase) == 75020.97600000002
+
+
+def test_unipolar_phase():
+    """ Test ex_exp_phase_calibration: unipolar phase nanoloop """
+
+    treat_phase, result = \
+        ex_exp_phase_calibration(type_of_data='unipolar', pha_corr='offset')
+
+    # print(sum(treat_phase))
+
+    assert result['corr'] == 'offset'
+    assert result['reverse'] is True
+    assert result['coefs'][0] == -1
+    assert result['coefs'][1] == approx(123.487975)
+    assert result['dict phase target']['low'] == 0
+    assert result['dict phase target']['high'] == 180
+    assert result['dict phase meas']['low'] == approx(123.487975)
+    assert result['dict phase meas']['high'] == approx(-56.512025)
+    assert sum(treat_phase) == 143649.849
+
+
+def test_phase_up_down():
+    """ Test ex_simulated_phase_calibration: up and down """
+
+    treat_phase, result, dict_pha = \
+        ex_simulated_phase_calibration(pha_corr='up_down')
 
     # print(sum(treat_phase['on']))
     # print(sum(treat_phase['off']))
@@ -44,9 +86,10 @@ def test_phase_up_down():
 
 
 def test_phase_affine():
-    """ Test ex_phase_calibration: affine """
+    """ Test ex_simulated_phase_calibration: affine """
 
-    treat_phase, result, dict_pha = ex_phase_calibration(pha_corr='affine')
+    treat_phase, result, dict_pha = \
+        ex_simulated_phase_calibration(pha_corr='affine')
 
     # print(sum(treat_phase['on']))
     # print(sum(treat_phase['off']))
@@ -83,9 +126,10 @@ def test_phase_affine():
 
 
 def test_phase_offset():
-    """ Test ex_phase_calibration: offset """
+    """ Test ex_simulated_phase_calibration: offset """
 
-    treat_phase, result, dict_pha = ex_phase_calibration(pha_corr='offset')
+    treat_phase, result, dict_pha = \
+        ex_simulated_phase_calibration(pha_corr='offset')
 
     # print(sum(treat_phase['on']))
     # print(sum(treat_phase['off']))
@@ -122,9 +166,10 @@ def test_phase_offset():
 
 
 def test_raw_phase():
-    """ Test ex_phase_calibration: raw """
+    """ Test ex_simulated_phase_calibration: raw """
 
-    treat_phase, result, dict_pha = ex_phase_calibration(pha_corr='raw')
+    treat_phase, result, dict_pha = \
+        ex_simulated_phase_calibration(pha_corr='raw')
 
     # print(sum(treat_phase['on']))
     # print(sum(treat_phase['off']))
@@ -143,3 +188,39 @@ def test_raw_phase():
 
     assert result['off']['corr'] == 'raw'
     assert sum(treat_phase['off']) == approx(174772.7191857715)
+
+
+def test_bipolar_phase_offset_determination():
+    """ Test ex_phase_offset_determination: bipolar phase nanoloop """
+
+    out = ex_phase_offset_determination(type_of_data='bipolar')
+    (phase_offset_val_dict, mean_phase_offset_val) = out
+
+    # print(np.sum(offset_phase))
+
+    assert phase_offset_val_dict['off'] == approx(79.80492244897957)
+    assert phase_offset_val_dict['on'] == approx(-90.6332)
+    assert mean_phase_offset_val == approx(85.21906122448979)
+
+
+def test_unipolar_phase_offset_determination():
+    """ Test ex_phase_offset_determination: unipolar phase nanoloop """
+
+    out = ex_phase_offset_determination(type_of_data='unipolar')
+    (phase_offset_val_dict, mean_phase_offset_val) = out
+
+    # print(np.sum(offset_phase))
+
+    assert phase_offset_val_dict['off'] == approx(-56.512025)
+    assert phase_offset_val_dict['on'] == approx(36.94068)
+    assert mean_phase_offset_val == approx(46.726352500000004)
+
+
+def test_apply_phase_offset():
+    """ Test ex_apply_phase_offset """
+
+    offset_phase = ex_apply_phase_offset()
+
+    # print(np.sum(offset_phase))
+
+    assert np.sum(offset_phase) == approx(16236457.717641905)
