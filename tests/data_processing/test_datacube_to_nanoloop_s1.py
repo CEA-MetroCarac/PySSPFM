37d426df4aa2a02209128022bff37ce2efd13c1d
yudgugger <yudgugger@gmail.com>
2024-03-11 14:35:07 +0100

    Major change: Add an optional offset phase applicable on all initial phase values: allows to correct unwanted phase switches that noise nanoloops (datacube_to_nanoloop_s1.py --> with the use of the apply_phase_offset function) 3 possible modes for phase offset correction: dynamic (phase offset is iterated with each new file) // static (unique phase offset value for all files), None (no offset correction applied) (datacube_to_nanoloop_s1.py) Extraction of other properties and save it (extract_other_properties) Added butterworth filters for segment processing (datacube_to_nanoloop_s1.py --> with use of butter_filter function) Processing of the different segments taken into account in datacube_to_nanoloop/analysis.py
    Minor change:
    Take in consideration real measurement time and save it (get_acquisition_time)
    Analysis of file in ascending order (get_file_names)
    Print file index
    Bug correction in phase extension extraction

--- tests/data_processing/test_datacube_to_nanoloop_s1.py
+++ tests/data_processing/test_datacube_to_nanoloop_s1.py
@@ -2,6 +2,8 @@
 Test datacube_to_nanoloop_s1 methods
 """
 
+from pytest import approx
+
 from examples.data_processing.ex_datacube_to_nanoloop_s1 import ex_single_script
 
 
@@ -11,4 +13,6 @@ from examples.data_processing.ex_datacube_to_nanoloop_s1 import ex_single_script
 def test_single_script():
     """ Test ex_single_script """
 
-    ex_single_script()
+    phase_offset_val = ex_single_script()
+    assert phase_offset_val['On field'] == approx(89, rel=3e-2)
+    assert phase_offset_val['Off field'] == approx(89, rel=3e-2)
