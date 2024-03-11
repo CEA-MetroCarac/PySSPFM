5d90fc493f9606f3a8814d1a7bf744aa39cbd823
yudgugger <yudgugger@gmail.com>
2024-03-11 15:10:59 +0100

    Remove mark slow

--- tests/data_processing/test_nanoloop_to_hyst_s2.py
+++ tests/data_processing/test_nanoloop_to_hyst_s2.py
@@ -9,7 +9,6 @@ from examples.data_processing.ex_nanoloop_to_hyst_s2 import ex_multi_script
 # class TestMain(unittest.TestCase):
 
 
-@pytest.mark.slow
 def test_multi_script():
     """ Test ex_multi_script """
 
