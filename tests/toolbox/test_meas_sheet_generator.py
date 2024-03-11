4a8f5dc6f646bedc0523cec79ace455623c0bd62
yudgugger <yudgugger@gmail.com>
2024-03-11 13:13:17 +0100

    test for meas_sheet_generator.py: allows to generate a pre-populated csv measurement sheet from an existing sheet and data contained in an SPM file

---
+++ tests/toolbox/test_meas_sheet_generator.py
@@ -0,0 +1,45 @@
+"""
+Test meas_sheet_generator methods
+"""
+from pytest import approx, skip
+
+from examples.toolbox.ex_meas_sheet_generator import \
+    example_meas_sheet_generator
+from PySSPFM.utils.raw_extraction import NanoscopeError
+
+
+# class TestSignalViewer(unittest.TestCase):
+
+
+def test_meas_sheet_generator():
+    """ Test example_meas_sheet_generator """
+    try:
+        indexs, values = example_meas_sheet_generator()
+    except NanoscopeError:
+        skip("Test skipped (NanoscopeError): spm file can't be opened without "
+             "Nanoscope Analysis DLL")
+
+    indexs_target = ['B4', 'C4', 'D4', 'B16', 'C16', 'D16', 'E16', 'F16',
+                     'G16', 'H16', 'I16', 'J16', 'K16', 'L16', 'M16', 'N16',
+                     'O16', 'P16', 'Q16', 'D22', 'F22']
+
+    assert indexs == indexs_target
+    assert values[0] == 'PIT_SSPFM_DFRT_T2ms_map.0_00000.spm'
+    assert values[1] == 'PIT_SSPFM_DFRT_T2ms_map.0_00000.spm'
+    assert values[2] == '14h : 16/02/2024'
+    assert values[3] == -10.0
+    assert values[4] == 10.0
+    assert values[5] == 50
+    assert values[6] == 'Zero, up'
+    assert values[7] == approx(1.9999999949504854)
+    assert values[8] == 100
+    assert values[9] == 0.0
+    assert values[10] == 0.0
+    assert values[11] == 10
+    assert values[12] == 'Single Read Step'
+    assert values[13] == approx(1.9999999949504854)
+    assert values[14] == 100
+    assert values[15] == approx(224.9799994319801)
+    assert values[16] == 2183
+    assert values[17] == approx(124.97999968445583)
+    assert values[18] == 2083
