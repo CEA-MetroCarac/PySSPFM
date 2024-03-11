6ec53fef1ad55f042dfa79e2c6c4257a5d4528a3
yudgugger <yudgugger@gmail.com>
2024-03-11 11:20:13 +0100

    Added butterworth filters for segment processing (with the use of the butter_filter function)

--- tests/utils/core/test_noise.py
+++ tests/utils/core/test_noise.py
@@ -4,7 +4,8 @@ Test noise methods
 from pytest import approx
 import numpy as np
 
-from examples.utils.core.ex_noise import ex_gen_noise, ex_filter_mean
+from examples.utils.core.ex_noise import \
+    ex_gen_noise, ex_filter_mean, ex_butter_filter
 
 
 def test_ex_gen_noise():
@@ -28,3 +29,35 @@ def test_ex_filter_mean():
     y_filt = ex_filter_mean()
 
     assert np.sum(y_filt) == approx(52.48731220242615)
+
+
+def test_ex_butter_filter_low():
+    """ Test ex_butter_filter low pass """
+
+    y_filt_low = ex_butter_filter(filter_type='low')
+
+    assert np.sum(y_filt_low) == approx(21.049807430587258)
+
+
+def test_ex_butter_filter_high():
+    """ Test ex_butter_filter high pass """
+
+    y_filt_high = ex_butter_filter(filter_type='high')
+
+    assert np.sum(y_filt_high) == approx(0.07489646793298177)
+
+
+def test_ex_butter_filter_bandpass():
+    """ Test ex_butter_filter band pass """
+
+    y_filt_bandpass = ex_butter_filter(filter_type='bandpass')
+
+    assert np.sum(y_filt_bandpass) == approx(-24.70597466183571)
+
+
+def test_ex_butter_filter_bandstop():
+    """ Test ex_butter_filter band stop """
+
+    y_filt_bandstop = ex_butter_filter(filter_type='bandstop')
+
+    assert np.sum(y_filt_bandstop) == approx(33.83620308961862)
