67d1ce0543a15aa93366ef1f40919a99bc0c5983
yudgugger <yudgugger@gmail.com>
2024-05-21 10:37:38 +0200

    Adapt docstring

--- PySSPFM/toolbox/spm_converter.py
+++ PySSPFM/toolbox/spm_converter.py
@@ -201,14 +201,22 @@ def main_spm_converter(dir_path_in, mode='classic', extension='txt',
 
 
 def main(fname_json=None):
-    """ Main function for data analysis """
+    """
+    Main function for data analysis.
+
+    fname_json: str
+        Path to the JSON file containing user parameters. If None,
+        the file is created in a default path:
+        (your_user_disk_access/.pysspfm/script_name_params.json)
+    """
     if get_setting("extract_parameters") in ['json', 'toml']:
         # if fname_json is provided, use it, else use the default one
         if fname_json is not None:
             file_path_user_params = fname_json
         else:
             file_path = os.path.realpath(__file__)
-            file_path_user_params = copy_default_settings_if_not_exist(file_path)
+            file_path_user_params = \
+                copy_default_settings_if_not_exist(file_path)
 
         # Load parameters from the specified configuration file
         print(f"user parameters from {os.path.split(file_path_user_params)[1]} "
