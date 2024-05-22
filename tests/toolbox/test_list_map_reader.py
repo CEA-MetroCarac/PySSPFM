db3686a42c7400d8844821df1e607640ec532c2c
yudgugger <yudgugger@gmail.com>
2024-05-22 10:00:20 +0200

    - Added the ability to extract and map new properties such as: 	- The phase offset from the phase_offset.txt file generated after running phase_offset_analyzer.py (PySSPFM/toolbox/phase_offset_analyzer.py) 	- The phase inversion from the phase_inversion.txt file generated after running phase_inversion_analyzer.py (PySSPFM/toolbox/phase_inversion_analyzer.py) 	- For this: adaptation of the extract_properties function in PySSPFM/utils/nanoloop_to_hyst/file.py - Added a histogram plot for the executable file PySSPFM/toolbox/list_map_reader.py

--- tests/toolbox/test_list_map_reader.py
+++ tests/toolbox/test_list_map_reader.py
@@ -2,15 +2,33 @@
 Test list_map_reader methods
 """
 
-from examples.toolbox.ex_list_map_reader import ex_list_map_reader
+from examples.toolbox.ex_list_map_reader import \
+    (ex_list_map_reader_properties, ex_list_map_reader_phase_inversion,
+     ex_list_map_reader_phase_offset)
 
 
 # class TestReaderListMaps(unittest.TestCase):
 
 
-def test_list_map_reader():
-    """ Test ex_list_map_reader """
+def test_list_map_reader_properties():
+    """ Test ex_list_map_reader_properties """
 
-    figures = ex_list_map_reader()
+    figures = ex_list_map_reader_properties()
+
+    assert len(list(figures)) == 5
+
+
+def test_list_map_reader_phase_inversion():
+    """ Test ex_list_map_reader_phase_inversion """
+
+    figures = ex_list_map_reader_phase_inversion()
+
+    assert len(list(figures)) == 4
+
+
+def test_list_map_reader_phase_offset():
+    """ Test ex_list_map_reader_phase_offset """
+
+    figures = ex_list_map_reader_phase_offset()
 
     assert len(list(figures)) == 4
