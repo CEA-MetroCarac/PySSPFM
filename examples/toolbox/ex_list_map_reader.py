43c1e546e9a7237b24e08cdd0d05b34b637d9caa
yudgugger <yudgugger@gmail.com>
2024-05-22 10:00:05 +0200

    - Added the ability to extract and map new properties such as: 	- The phase offset from the phase_offset.txt file generated after running phase_offset_analyzer.py (PySSPFM/toolbox/phase_offset_analyzer.py) 	- The phase inversion from the phase_inversion.txt file generated after running phase_inversion_analyzer.py (PySSPFM/toolbox/phase_inversion_analyzer.py) 	- For this: adaptation of the extract_properties function in PySSPFM/utils/nanoloop_to_hyst/file.py

--- examples/toolbox/ex_list_map_reader.py
+++ examples/toolbox/ex_list_map_reader.py
@@ -10,9 +10,9 @@ from PySSPFM.utils.core.figure import print_plots
 from PySSPFM.toolbox.list_map_reader import main_list_map_reader
 
 
-def ex_list_map_reader(verbose=False):
+def ex_list_map_reader_properties(verbose=False):
     """
-    Example of list_map_reader functions.
+    Example of list_map_reader functions for properties directory.
 
     Parameters
     ----------
@@ -54,11 +54,97 @@ def ex_list_map_reader(verbose=False):
     return figures
 
 
+def ex_list_map_reader_phase_inversion(verbose=False):
+    """
+    Example of list_map_reader functions for phase inversion directory.
+
+    Parameters
+    ----------
+    verbose: bool, optional
+        Verbosity flag (default is False).
+
+    Returns
+    -------
+    figures: list
+        List of figures.
+    """
+    example_root_path_in = get_setting("example_root_path_in")
+    dir_path_in = os.path.join(
+        example_root_path_in, "KNN500n_toolbox",
+        "phase_inversion_analyzer_2024-05-21-18h13m")
+
+    ind_maps = [['phase_inversion', 'Grad On field'],
+                ['phase_inversion', 'Grad Off field'],
+                ['phase_inversion', 'Revert On Off']]
+
+    user_pars = {'interp fact': 1,
+                 'interp func': 'linear',
+                 'meas time': 1.5,
+                 'revert mask': False,
+                 'man mask': [],
+                 'ref': {'mode': 'off',
+                         'prop': 'charac tot fit: R_2 hyst',
+                         'fmt': '.5f',
+                         'min val': 0.90,
+                         'max val': None,
+                         'interactive': False},
+                 'ind maps': ind_maps}
+
+    # ex main_list_map_reader
+    figures = main_list_map_reader(user_pars, dir_path_in, verbose=verbose)
+
+    return figures
+
+
+def ex_list_map_reader_phase_offset(verbose=False):
+    """
+    Example of list_map_reader functions for phase offset directory.
+
+    Parameters
+    ----------
+    verbose: bool, optional
+        Verbosity flag (default is False).
+
+    Returns
+    -------
+    figures: list
+        List of figures.
+    """
+    example_root_path_in = get_setting("example_root_path_in")
+    dir_path_in = os.path.join(
+        example_root_path_in, "KNN500n_toolbox",
+        "phase_offset_analyzer_2024-05-21-18h18m")
+
+    ind_maps = [['phase_offset', 'On field'],
+                ['phase_offset', 'Off field'],
+                ['phase_offset', 'Mean']]
+
+    user_pars = {'interp fact': 1,
+                 'interp func': 'linear',
+                 'meas time': 1.5,
+                 'revert mask': False,
+                 'man mask': [],
+                 'ref': {'mode': 'off',
+                         'prop': 'charac tot fit: R_2 hyst',
+                         'fmt': '.5f',
+                         'min val': 0.90,
+                         'max val': None,
+                         'interactive': False},
+                 'ind maps': ind_maps}
+
+    # ex main_list_map_reader
+    figures = main_list_map_reader(user_pars, dir_path_in, verbose=verbose)
+
+    return figures
+
+
 if __name__ == '__main__':
     # saving path management
     dir_path_out, save_plots = save_path_example(
         "list_map_reader", save_example_exe=True, save_test_exe=False)
     figs = []
-    figs += ex_list_map_reader(verbose=True)
+    figs += ex_list_map_reader_properties(verbose=True)
+    figs += ex_list_map_reader_phase_inversion(verbose=True)
+    figs += ex_list_map_reader_phase_offset(verbose=True)
     print_plots(figs, save_plots=save_plots, show_plots=True,
                 dirname=dir_path_out, transparent=False)
