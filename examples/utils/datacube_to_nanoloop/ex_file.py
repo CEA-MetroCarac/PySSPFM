8bb6dfbbb9cdc383055208735d6a37f1cb25062f
yudgugger <yudgugger@gmail.com>
2024-06-21 17:57:10 +0200

    Update 2024.07 Adapt example to 2024.07 update Remove : 	- save_parameters 	- get_acquisition_time

--- examples/utils/datacube_to_nanoloop/ex_file.py
+++ examples/utils/datacube_to_nanoloop/ex_file.py
@@ -3,14 +3,12 @@ Example of file methods
 """
 import os
 import shutil
-import time
-from datetime import datetime
 
 from examples.utils.datacube_to_nanoloop.ex_gen_data import pars_segment
 from PySSPFM.settings import get_setting
 from PySSPFM.utils.raw_extraction import csv_meas_sheet_extract
 from PySSPFM.utils.datacube_to_nanoloop.file import \
-    print_params, save_parameters, get_acquisition_time, get_phase_tab_offset
+    get_phase_tab_offset, print_params
 
 
 def example_file(verbose=False):
@@ -25,10 +23,8 @@ def example_file(verbose=False):
 
     Returns
     -------
-    exp_meas_time: float
-        Experimental acquisition time in seconds
-    phase_tab: array-like
-        Phase offset table extracted from the file
+    phase_tab : array-like
+        Phase offset table extracted from a text file.
     """
     # In and out file management
     dir_path_in = os.path.join(get_setting("example_root_path_in"), "KNN500n")
@@ -47,9 +43,6 @@ def example_file(verbose=False):
     if os.path.isdir(dir_path_out):
         shutil.rmtree(dir_path_out)
 
-    t0, date = time.time(), datetime.now()
-    date = date.strftime('%Y-%m-%d %H;%M')
-
     # Get parameters
     _, _, _, _, _, _, user_pars = pars_segment()
     user_pars['f path'] = dir_path_in
@@ -60,20 +53,10 @@ def example_file(verbose=False):
         print('- ex print_params:')
     print_params(meas_pars, sign_pars, user_pars, verbose=verbose)
 
-    # ex get_acquisition_time
-    exp_meas_time = get_acquisition_time(dir_path_in, file_format='.txt')
-    if verbose:
-        print('\n- ex get_acquisition_time:')
-        print(f'experimental acquisition time [s]: {exp_meas_time}')
-
-    # ex save_parameters
-    save_parameters(dir_path_out, t0, date, exp_meas_time, user_pars, meas_pars,
-                    sign_pars, 0)
-
     # ex get_phase_tab_offset
     phase_tab = get_phase_tab_offset(file_path_in_offset)
 
-    return exp_meas_time, phase_tab
+    return phase_tab
 
 
 if __name__ == '__main__':
