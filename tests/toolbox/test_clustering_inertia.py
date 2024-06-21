c1e27fbd2476ecc23842bc25ae265023d180c5ec
yudgugger <yudgugger@gmail.com>
2024-06-21 17:38:20 +0200

    Update 2024.07 Clustering inertia : Determines the inertia based on the number of clusters used in order to determine the optimal number of clusters. test file

---
+++ tests/toolbox/test_clustering_inertia.py
@@ -0,0 +1,25 @@
+"""
+Test clustering_inertia methods
+"""
+
+from pytest import approx
+import numpy as np
+
+from examples.toolbox.ex_clustering_inertia import ex_clustering_inertia
+
+
+# class TestReaderAllMaps(unittest.TestCase):
+
+
+def test_clustering_inertia():
+    """ Test ex_clustering_inertia for piezoresponse signal """
+
+    dict_inertia = ex_clustering_inertia()
+
+    # print(np.sum(dict_inertia["off"]))
+    # print(np.sum(dict_inertia["on"]))
+    # print(np.sum(dict_inertia["coupled"]))
+
+    assert np.sum(dict_inertia["off"]) == approx(0.8467106430607041)
+    assert np.sum(dict_inertia["on"]) == approx(0.7018673467173553)
+    assert np.sum(dict_inertia["coupled"]) == approx(1.1124421123631292)
