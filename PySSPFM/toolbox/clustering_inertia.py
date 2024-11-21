2e90c8c11cb31291d9c803c852fcd6a1e40f506d
yudgugger <yudgugger@gmail.com>
2024-11-21 17:51:43 +0100

    Update 2024.11
    # Clustering vector / curve :
    - Changes mainly affect the following scripts: PySSPFM/toolbox/curve_clustering.py and PySSPFM/toolbox/clustering_inertia.py.
    - Renaming: PySSPFM/toolbox/vector_clustering.py → PySSPFM/toolbox/curve_clustering.py.
    - Removal of differentiation between curves and loops: clustering now applies to a curve, which is a loop (obtained after the first stage of data processing). Force curve clustering is handled in another script. Functions, input parameters, etc., have been adapted or removed accordingly.
    - PCA is no longer optional and is always active.
    - Cluster labels are saved as properties (maps) along with average loops per cluster (x and y axes) in PySSPFM/toolbox/vector_clustering.py.
    - New plot_axis_y option for the perform_curve_clustering function to plot desired y-axis values (added for use in force curve clustering).
    - Bug fixes in the clustering executable script related to case differentiation.
    - Redefinition of the x-axis for averaged curves by cluster → previous determination of the x-axis was inaccurate in certain cases (e.g., force curves).
    
    # Updated normalization method for clustering curves with multiple magnitudes:
    - All curves are normalized using an affine relationship based on the normalization (0 to 1) of the average curve for each magnitude.
    - Plots have been updated accordingly.

--- PySSPFM/toolbox/clustering_inertia.py
+++ PySSPFM/toolbox/clustering_inertia.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 from PySSPFM.settings import get_setting, get_config
 from PySSPFM.utils.core.figure import print_plots, plot_graph
-from PySSPFM.toolbox.vector_clustering import main_vector_clustering
+from PySSPFM.toolbox.curve_clustering import main_loop_clustering
 from PySSPFM.utils.path_for_runable import \
     save_path_management, copy_json_res, create_json_res
 
@@ -46,23 +46,18 @@ def plot_inertia(tab_index_cluster, dict_inertia):
 
 
 def main_clustering_inertia(
-        user_pars, loop_pars, curve_pars, dir_path_in, verbose=False,
-        show_plots=True, save_plots=False, dir_path_out=None,
-        dir_path_in_props=None):
+        user_pars, dir_path_in, verbose=False, show_plots=True,
+        save_plots=False, dir_path_out=None, dir_path_in_props=None):
 
     """
-    Perform vector clustering analysis.
+    Perform curve clustering analysis.
 
     Parameters
     ----------
     user_pars : dict
         User parameters.
-    loop_pars : dict
-        User parameters for loop clustering analysis.
-    curve_pars : dict
-        User parameters for curve clustering analysis.
     dir_path_in : str
-        Path of vector (loop or curve) txt directory (in).
+        Path of curve txt directory (in).
     verbose : bool, optional
         Activation key for verbosity.
     show_plots : bool, optional
@@ -76,9 +71,9 @@ def main_clustering_inertia(
 
     Returns
     -------
-    dict
+    dict_inertia: dict
         Returns a dictionary containing inertia information for each clustering
-        mode and the average vector for each cluster.
+        mode and the average curve for each cluster.
     """
 
     tab_index_cluster = range(2, user_pars['lim cluster']+1)
@@ -90,27 +85,26 @@ def main_clustering_inertia(
             print(f"\nNumber of cluster: {index_cluster}\n")
 
         # Init cluster index
-        loop_pars['nb clusters off'] = index_cluster
-        loop_pars['nb clusters on'] = index_cluster
-        loop_pars['nb clusters coupled'] = index_cluster
-        curve_pars['nb clusters'] = index_cluster
+        user_pars['nb clusters off'] = index_cluster
+        user_pars['nb clusters on'] = index_cluster
+        user_pars['nb clusters coupled'] = index_cluster
 
         # Clustering
-        _, _, inertia, _ = main_vector_clustering(
-            user_pars, loop_pars, curve_pars, dir_path_in, verbose=verbose,
-            show_plots=False, save_plots=False, dir_path_out=dir_path_out,
+        _, _, inertia, _, _ = main_loop_clustering(
+            user_pars, dir_path_in, verbose=verbose,
+            show_plots=False, save=False, dir_path_out=dir_path_out,
             dir_path_in_props=dir_path_in_props)
 
         # Append inertia values
         if isinstance(inertia, dict):
-            for key in inertia.keys():
+            for key, value in inertia.items():
                 if cont == 0:
                     dict_inertia[key] = []
-                dict_inertia[key].append(inertia[key])
+                dict_inertia[key].append(value)
         else:
             if cont == 0:
                 dict_inertia['curve'] = []
-            dict_inertia.append(inertia)
+            dict_inertia['curve'].append(inertia)
 
     # Plots the inertia as a function of the number of clusters
     make_plots = bool(show_plots or save_plots)
@@ -134,23 +128,11 @@ def parameters(fname_json=None):
         the file is created in a default path:
         (your_user_disk_access/.pysspfm/script_name_params.json)
 
-    - object: str
-        Name of the Object Processed with Clustering Analysis
-        This parameter determines the name of the object used to perform the
-        clustering.
-        Implemented objects are Loops (best nanoloops associated with each
-        pixel) or Curves (raw SSPFM measurements associated with each
-        pixel).
-        Choose from: "loop", "curve"
     - relative: bool
         Activation key for relative clustering analysis.
         This parameter serves as an activation key to perform clustering
-        analysis on relative vectors (all vectors vary between 0 and 1).
-        Always active for combined vectors of multiple measurements.
-    - pca: bool
-        Activation key for performing PCA before clustering analysis.
-        This parameter serves as an activation key to perform PCA (Principal
-        Component Analysis) before clustering analysis.
+        analysis on relative curves (all curves vary between 0 and 1).
+        Always active for combined curves of multiple measurements.
     - method: str
         Name of the Method Used to Perform the Clustering
         This parameter determines the method used to perform the clustering.
@@ -161,7 +143,6 @@ def parameters(fname_json=None):
         Maximum limit of the number of clusters to determine inertia.
         This parameter sets the maximum number of clusters for which inertia
         values are calculated and associated.
-
     - label_meas: list of str
         List of Measurement Name for Loops
         This parameter contains a list of measurement name in order to create
@@ -170,39 +151,20 @@ def parameters(fname_json=None):
         normalized and concatenated.
         Choose from : piezoresponse, amplitude, phase, res freq and q fact
 
-    - extension: str, optional
-        Extension of files.
-        This parameter determines the extension type of curve files.
-        Four possible values: 'spm' or 'txt' or 'csv' or 'xlsx'.
-    - mode: str
-        Mode of measurement used (extraction of measurements).
-        This parameter determines the method used for measurements,
-        specifically for the extraction measurements.
-        Two possible values: 'classic' (sweep or single frequency) or 'dfrt'.
-    - label_meas: list of str
-        List of Measurement Name for Curves
-        This parameter contains a list of measurement name in order to create
-        the curve to be analyzed using a machine learning algorithm
-        of clustering. If several name are filled, the curve will be
-        normalized and concatenated.
-
     - dir_path_in: str
-        Input Directory for Vector Files (default: 'best_nanoloops').
-        This parameter specifies the directory path for the vector
+        Input Directory for Curves Files (default: 'best_nanoloops').
+        This parameter specifies the directory path for the curve
         files, to perform clustering analysis.
     - dir_path_out: str
-        Saving directory for analysis results figures
+        Saving directory for analysis results
         (optional, default: toolbox directory in the same root)
-        This parameter specifies the directory where the figures
-        generated as a result of the analysis will be saved.
+        This parameter specifies the directory where the results of the
+        analysis will be saved.
     - dir_path_in_props: str
         Properties files directory
         (optional, default: properties).
-        This parameter specifies the directory containing the properties
-        files.
-        For loop clustering : text file generated after the 2nd step
-        of the analysis.
-        For curve clustering : CSV measurement file (measurement sheet model).
+        This parameter specifies the directory containing the properties files,
+        which is a text file generated after the 2nd step of the analysis.
     - verbose: bool
         Activation key for printing verbosity during analysis.
         This parameter serves as an activation key for printing verbose
@@ -220,12 +182,12 @@ def parameters(fname_json=None):
         config_params, fname_json = get_config(__file__, fname_json)
     elif get_setting("extract_parameters") == 'python':
         print("user parameters from python file")
-        # Select vector folder
+        # Select curve folder
         dir_path_in = tkf.askdirectory()
         # dir_path_in = r'...\KNN500n_15h18m02-10-2023_out_dfrt\best_nanoloops
         dir_path_out = None
         # dir_path_out = r'...\KNN500n_15h18m02-10-2023_out_dfrt\toolbox\
-        # vector_clustering_2023-10-02-16h38m
+        # clustering_inertia_2023-10-02-16h38m
         dir_path_in_props = None
         # dir_path_in_props = r'...\KNN500n_15h18m02-10-2023_out_dfrt\properties
         config_params = {
@@ -236,27 +198,17 @@ def parameters(fname_json=None):
             "show_plots": True,
             "save": False,
             "user_pars": {
-                "object": "loop",
                 "relative": False,
-                "pca": True,
                 "method": "kmeans",
-                "lim cluster": 10
-            },
-            "loop_pars": {
+                "lim cluster": 10,
                 "label meas": ["piezoresponse"]
-            },
-            "curve_pars": {
-                "extension": "spm",
-                "mode": "classic",
-                "label meas": ["deflection"]
             }
         }
     else:
         raise NotImplementedError("setting 'extract_parameters' "
                                   "should be in ['json', 'toml', 'python']")
 
-    return config_params['user_pars'], config_params['loop_pars'], \
-        config_params['curve_pars'], config_params['dir_path_in'], \
+    return config_params['user_pars'], config_params['dir_path_in'], \
         config_params['dir_path_out'], config_params['dir_path_in_props'], \
         config_params['verbose'], config_params['show_plots'], \
         config_params['save'], fname_json, config_params
@@ -272,8 +224,8 @@ def main(fname_json=None):
         (your_user_disk_access/.pysspfm/script_name_params.json)
     """
     # Extract parameters
-    (user_pars, loop_pars, curve_pars, dir_path_in, dir_path_out,
-     dir_path_in_props, verbose, show_plots, save, fname_json,
+    (user_pars, dir_path_in, dir_path_out, dir_path_in_props,
+     verbose, show_plots, save, fname_json,
      config_params) = parameters(fname_json=fname_json)
     # Generate default path out
     dir_path_out = save_path_management(
@@ -281,8 +233,8 @@ def main(fname_json=None):
         lvl=1, create_path=True, post_analysis=True)
     # Main function
     main_clustering_inertia(
-        user_pars, loop_pars, curve_pars, dir_path_in, verbose=verbose,
-        show_plots=show_plots, save_plots=save, dir_path_out=dir_path_out,
+        user_pars, dir_path_in, verbose=verbose, show_plots=show_plots,
+        save_plots=save, dir_path_out=dir_path_out,
         dir_path_in_props=dir_path_in_props)
     # Save parameters
     if save:
