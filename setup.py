c129913704abda6ff59fde8419f6977238fedeb3
yudgugger <yudgugger@gmail.com>
2024-11-25 15:10:27 +0100

    Update 2024.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PySSPFM",
-    version='2024.03',
+    version='2024.11',
     license='GPL v3',
     include_package_data=True,
     zip_safe=False,
